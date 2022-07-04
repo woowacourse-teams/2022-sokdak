f222a60f55ddce2c5b7afad2d07673c202904bd7
EastHShin <xldk78@gmail.com>
2022-07-04 17:58:39 +0900

    project setting

---
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/SokdakApplication.java
@@ -0,0 +1,13 @@
+package com.wooteco.sokdak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokdakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokdakApplication.class, args);
+	}
+
+}

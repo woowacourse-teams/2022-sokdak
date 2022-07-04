4522cf1c4fc49e58e5f470d8db890a33ab537dc3
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-07-04 22:28:16 +0900

    feat: global style 적용
    - 폰트 추가
    
    Co-authored-by: JUDONGHYEOK <JUDONGHYEOK@users.noreply.github.com>

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,9 +1,20 @@
+import GlobalStyle from '../src/GlobalStyle';
+
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};

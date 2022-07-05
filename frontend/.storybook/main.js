2d9d67756cd74b90cc1babd3f400b30f85537457
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-07-05 22:04:22 +0900

    fix: storybook에서 routing 추가
    - storybook-addon-react-router-v6에서 withRouter 추가
    
    Co-authored-by: JUDONGHYEOK <JUDONGHYEOK@users.noreply.github.com>

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -2,7 +2,12 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-react-router-v6',
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',

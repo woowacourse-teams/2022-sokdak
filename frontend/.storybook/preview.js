757dde013a92474ae1d407be3300b34cb852d009
JUDONGHYEOK <judonghyeok@gmail.com>
2022-07-06 22:32:43 +0900

    fix: storybook router 및 msw 적용
    Co-authored-by: YoungHwa Byun <rlaalsdl111@naver.com>

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,17 +1,27 @@
 import { ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import GlobalStyle from '../src/style/GlobalStyle';
 import theme from '../src/style/theme';
+
+const queryClient = new QueryClient();
 
 export const decorators = [
   Story => (
     <>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Story />
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <Story />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   ),
 ];
+
+if (typeof global.process === 'undefined') {
+  const { worker } = require('../src/mocks/worker');
+  worker.start();
+}
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },

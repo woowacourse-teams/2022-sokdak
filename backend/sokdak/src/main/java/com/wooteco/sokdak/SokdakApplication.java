3926efaef7bec55d8a7895769177a077b1d03040
Chris <bjuuuu98@gmail.com>
2022-07-07 16:10:29 +0900

    refactor: JPA 관련 설정 분리

--- backend/sokdak/src/main/java/com/wooteco/sokdak/SokdakApplication.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/SokdakApplication.java
@@ -2,10 +2,8 @@ package com.wooteco.sokdak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SokdakApplication {
 
 	public static void main(String[] args) {

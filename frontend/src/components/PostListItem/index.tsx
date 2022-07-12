06a51dadfbafd17d495589b7224b1bc3ae7e4fda
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-07-12 20:12:17 +0900

    refactor: 커스텀 타입 네이밍 수정
    Co-authored-by: JUDONGHYEOK <JUDONGHYEOK@users.noreply.github.com>

--- frontend/src/components/PostListItem/index.tsx
+++ frontend/src/components/PostListItem/index.tsx
@@ -2,12 +2,12 @@ import * as Styled from './index.styles';
 import timeConverter from '@/utils/timeConverter';
 import { forwardRef } from 'react';
 
-interface PostListItemProp extends Omit<Post, 'id'> {
+interface PostListItemProps extends Omit<Post, 'id'> {
   handleClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-const PostListItem = forwardRef<HTMLDivElement, PostListItemProp>(
-  ({ title, localDate, content, handleClick }: PostListItemProp, ref) => {
+const PostListItem = forwardRef<HTMLDivElement, PostListItemProps>(
+  ({ title, localDate, content, handleClick }: PostListItemProps, ref) => {
     return (
       <Styled.Container onClick={handleClick} ref={ref}>
         <Styled.TitleContainer>

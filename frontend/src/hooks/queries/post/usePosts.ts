7f107c499cc9d845536f5025622ae5ecdab7b169
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-07-13 18:08:14 +0900

    refactor: API 로직을 custom hook으로 분리
    Co-authored-by: JUDONGHYEOK <JUDONGHYEOK@users.noreply.github.com>

---
+++ frontend/src/hooks/queries/post/usePosts.ts
@@ -0,0 +1,37 @@
+import { useInfiniteQuery, QueryKey, UseInfiniteQueryOptions } from 'react-query';
+
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+import QUERY_KEYS from '@/constants/queries';
+
+const usePosts = ({
+  storeCode,
+  options,
+}: {
+  storeCode: QueryKey;
+  options?: UseInfiniteQueryOptions<
+    AxiosResponse<{ posts: Post; lastPage: boolean }>,
+    AxiosError,
+    Post,
+    AxiosResponse<{ posts: Post; lastPage: boolean }>,
+    QueryKey[]
+  >;
+}) =>
+  useInfiniteQuery(
+    [QUERY_KEYS.POSTS, storeCode],
+    ({ pageParam = 0, queryKey: [_, size] }) => axios.get(`/posts?size=${size}&page=${pageParam}`),
+    {
+      select: data => {
+        const posts: Post[] = data.pages.reduce((pre, { data }) => pre.concat(data.posts), [] as Post[]);
+        return {
+          pages: posts,
+          pageParams: [...data.pageParams, data.pageParams.length],
+        };
+      },
+      enabled: false,
+      getNextPageParam: (lastPage, allPages) => (lastPage.data.lastPage ? undefined : allPages.length),
+      ...options,
+    },
+  );
+
+export default usePosts;

1e30516b1d8d82fecbe6e8dce62aa50558e6397d
JUDONGHYEOK <judonghyeok@gmail.com>
2022-07-14 21:43:53 +0900

    feat: ID Input PasswordInput 컴포넌트 구현

---
+++ frontend/src/pages/LoginPage/components/IDInput/index.tsx
@@ -0,0 +1,20 @@
+import InputBox from '@/components/@shared/InputBox';
+import { useInput } from '@/components/@shared/InputBox/useInput';
+
+const IDInput = ({ value, setValue, error, setError }: ReturnType<typeof useInput>) => {
+  return (
+    <InputBox value={value} setValue={setValue} error={error} setError={setError}>
+      <InputBox.Input
+        type="text"
+        placeholder="아이디"
+        handleInvalid={() => {
+          setError('정확한 아이디 형식을 입력해주세요');
+        }}
+        required
+      />
+      <InputBox.ErrorMessage />
+    </InputBox>
+  );
+};
+
+export default IDInput;

7de3ed84d6f8df5c45e5e3e2210b9fe92ac5ba44
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-07-14 23:34:22 +0900

    feat: UpdatePostPage를 routes에 추가

--- frontend/src/constants/snackbar.ts
+++ frontend/src/constants/snackbar.ts
@@ -4,6 +4,7 @@ const SNACKBAR_MESSAGE = {
   SUCCESS_DELETE_POST: '해당 게시물이 삭제되었습니다.',
   SUCCESS_LOGIN: '로그인에 성공하였습니다.',
   FAIL_LOGIN: '아이디와 비밀번호를 확인해주세요',
+  SUCCESS_UPDATE_POST: '글 수정에 성공하였습니다.',
 };
 
 export default SNACKBAR_MESSAGE;

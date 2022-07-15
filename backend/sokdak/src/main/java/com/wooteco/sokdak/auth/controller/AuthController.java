0bffff3b8c43a3a8239b8a8497fc0a60eb0406d4
hyunrrr <whgusrms96@gmail.com>
2022-07-15 19:58:48 +0900

    chore: 검토용 코드 삭제

--- backend/sokdak/src/main/java/com/wooteco/sokdak/auth/controller/AuthController.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/auth/controller/AuthController.java
@@ -6,11 +6,9 @@ import com.wooteco.sokdak.auth.service.AuthService;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RestController
 public class AuthController {
@@ -25,19 +23,6 @@ public class AuthController {
     public ResponseEntity<Void> login(HttpSession httpSession, @Valid @RequestBody LoginRequest loginRequest) {
         AuthInfo authInfo = authService.login(loginRequest);
         httpSession.setAttribute("member", authInfo);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/test")
-    public ResponseEntity<Void> testSession(@SessionAttribute(name = "member") AuthInfo authInfo) {
-        System.err.println("성공 authInfo = " + authInfo);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/test2")
-    public ResponseEntity<Void> testSession2(HttpSession httpSession) {
-        AuthInfo authInfo = (AuthInfo) httpSession.getAttribute("member");
-        System.out.println(authInfo.getId() + "!!!!!");
         return ResponseEntity.ok().build();
     }
 }

f1569122733e451f40023496bd1dfc1d6693066c
Chris <bjuuuu98@gmail.com>
2022-07-15 21:41:10 +0900

    refactor(PostRepository): 테스트케에스 BeforeEach로 분리

--- backend/sokdak/src/test/java/com/wooteco/sokdak/post/repository/PostRepositoryTest.java
+++ backend/sokdak/src/test/java/com/wooteco/sokdak/post/repository/PostRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.wooteco.sokdak.post.domain.Post;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,48 +18,31 @@ class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
 
-    @DisplayName("특정 페이지의 게시글들을 가져오는지 확인한다.")
-    @Test
-    void findWithPagination() {
-        Post post1 = Post.builder()
+    private Post post1;
+    private Post post2;
+    private Post post3;
+    private Post post4;
+    private Post post5;
+
+    @BeforeEach
+    void setUp() {
+        post1 = Post.builder()
                 .title("제목1")
                 .content("본문1")
                 .build();
-        Post post2 = Post.builder()
+        post2 = Post.builder()
                 .title("제목2")
                 .content("본문2")
                 .build();
-        postRepository.save(post1);
-        postRepository.save(post2);
-
-        Slice<Post> result = postRepository.findSliceBy(PageRequest.of(0, 3));
-
-        assertAll(
-                () -> assertThat(result).containsExactly(post1, post2),
-                () -> assertThat(result.isLast()).isTrue()
-        );
-    }
-
-    @DisplayName("특정 페이지의 게시글들을 가져오는지 확인한다.")
-    @Test
-    void findWithPagination2() {
-        Post post1 = Post.builder()
-                .title("제목1")
-                .content("본문1")
-                .build();
-        Post post2 = Post.builder()
-                .title("제목2")
-                .content("본문2")
-                .build();
-        Post post3 = Post.builder()
+        post3 = Post.builder()
                 .title("제목3")
                 .content("본문3")
                 .build();
-        Post post4 = Post.builder()
+        post4 = Post.builder()
                 .title("제목4")
                 .content("본문4")
                 .build();
-        Post post5 = Post.builder()
+        post5 = Post.builder()
                 .title("제목5")
                 .content("본문5")
                 .build();
@@ -67,7 +51,22 @@ class PostRepositoryTest {
         postRepository.save(post3);
         postRepository.save(post4);
         postRepository.save(post5);
+    }
 
+    @DisplayName("특정 페이지의 게시글들을 가져오는지 확인한다.")
+    @Test
+    void findWithPagination() {
+        Slice<Post> result = postRepository.findSliceBy(PageRequest.of(0, 6));
+
+        assertAll(
+                () -> assertThat(result).containsExactly(post1, post2, post3, post4, post5),
+                () -> assertThat(result.isLast()).isTrue()
+        );
+    }
+
+    @DisplayName("특정 페이지의 게시글들을 가져오는지 확인한다.")
+    @Test
+    void findWithPagination2() {
         Slice<Post> result = postRepository.findSliceBy(PageRequest.of(1, 2));
 
         assertAll(

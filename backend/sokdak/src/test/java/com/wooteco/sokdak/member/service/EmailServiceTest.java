e7ec4e9d86be9511ed06d6737729a96ab9d712c1
injoon2019 <ison@sfu.ca>
2022-07-17 21:13:23 +0900

    refactor: EmailService 테스트 코드 작성

--- backend/sokdak/src/test/java/com/wooteco/sokdak/member/service/EmailServiceTest.java
+++ backend/sokdak/src/test/java/com/wooteco/sokdak/member/service/EmailServiceTest.java
@@ -1,10 +1,16 @@
 package com.wooteco.sokdak.member.service;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import com.wooteco.sokdak.auth.domain.AuthCode;
 import com.wooteco.sokdak.auth.domain.Ticket;
+import com.wooteco.sokdak.auth.service.AuthCodeGenerator;
+import com.wooteco.sokdak.auth.service.Encryptor;
 import com.wooteco.sokdak.member.dto.EmailRequest;
+import com.wooteco.sokdak.member.repository.AuthCodeRepository;
 import com.wooteco.sokdak.member.repository.TicketRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,50 +23,51 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 class EmailServiceTest {
 
-//    @Autowired
-//    AuthCodeRepository authCodeRepository;
-
-//    @Autowired
-//    TicketRepository ticketRepository;
+    @Autowired
+    private EmailService emailService;
 
     @Autowired
-    EmailService emailService;
+    private TicketRepository ticketRepository;
 
     @Autowired
-    EmailSender emailSender;
+    private AuthCodeRepository authCodeRepository;
 
     @MockBean
-    TicketRepository ticketRepository;
+    private AuthCodeGenerator authCodeGenerator;
 
-//    @BeforeEach
-//    void setUp() {
-//        Ticket ticket = Ticket.builder()
-//                .serialNumber("bjuuuu98@gmail.com")
-//                .used(false)
-//                .build();
-//        ticketRepository.save(ticket);
-//    }
+    @MockBean
+    private Encryptor encryptor;
 
-    // TODO: 실제 이메일 보내는 기능은 모킹하고 검증하는 로직 테스트 하도록 변경
-    @DisplayName("특정 이메일로 인증코드 발송")
+    @MockBean
+    private EmailSender emailSender;
+
+    @DisplayName("인증 코드 저장")
     @Test
-    void sendEmail() {
-        Ticket ticket = Ticket.builder().serialNumber("serialNumber").used(false).build();
+    void createAndSaveAuthCode() {
+        given(authCodeGenerator.generate(6))
+                .willReturn("a1b1c1");
 
-        TicketRepository ticketRepository = mock(TicketRepository.class);
-        EmailRequest emailRequest = new EmailRequest("bjuuuu98@gmail.com");
-//        String serialNumber = Encryptor.encrypt(emailRequest.getEmail());
-//
-//        when(ticketRepository.existsBySerialNumber(anyString()))
-//                .thenReturn(true);
-//
-//        when(ticketRepository.findBySerialNumber(anyString()))
-//                .thenReturn(Optional.ofNullable(ticket));
-//
-//        when(emailService.getAuthCode(emailRequest))
-//                .thenReturn("a1b2c3");
+        emailService.createAndSaveAuthCode("serialNumber");
+        AuthCode authCode = authCodeRepository.findBySerialNumber("serialNumber").get();
 
-        emailService.sendCodeToValidUser(emailRequest);
-        verify(emailSender).send("bjuuuu98@gmail.com", "a1b2c3");
+        assertThat(authCode.getCode()).isEqualTo("a1b1c1");
+    }
+
+    @DisplayName("인증 코드 발송")
+    @Test
+    void sendCodeToValidUser() {
+        given(encryptor.encrypt("test@gmail.com"))
+                .willReturn("serialNumber");
+        given(authCodeGenerator.generate(6))
+                .willReturn("a1b1c1");
+        Ticket ticket = Ticket.builder()
+                .serialNumber("serialNumber")
+                .used(false)
+                .build();
+        ticketRepository.save(ticket);
+
+        emailService.sendCodeToValidUser(new EmailRequest("test@gmail.com"));
+
+        verify(emailSender, times(1)).send("test@gmail.com", "a1b1c1");
     }
 }

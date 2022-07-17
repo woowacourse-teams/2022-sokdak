361f31b274c43b2a1c749865a25c691102d56ed8
Chris <bjuuuu98@gmail.com>
2022-07-17 22:39:56 +0900

    test: 여러 컨트롤러 테스트를 위한 ControllerTest 추가

--- backend/sokdak/src/test/resources/post_test.sql
+++ backend/sokdak/src/test/resources/post_test.sql
@@ -1,2 +1,1 @@
-insert into member (username, nickname, password)
-values ('chris', 'chrisNickname', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e')
+insert into member (username, nickname, password) values ('chris', 'chrisNickname', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e')

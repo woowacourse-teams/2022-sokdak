9efe6d7ab1e894be147ee3dfd2b4191446b25899
JUDONGHYEOK <judonghyeok@gmail.com>
2022-07-19 17:11:28 +0900

    feat: 댓글 불러오기 기능 추가

--- frontend/src/constants/queries.ts
+++ frontend/src/constants/queries.ts
@@ -3,6 +3,7 @@ const QUERY_KEYS = {
   POSTS: 'posts',
   MEMBER_ID_CHECK: 'member-id-check',
   MEMBER_NICKNAME_CHECK: 'member-nickname-check',
+  COMMENTS: 'comments',
 };
 
 export default QUERY_KEYS;

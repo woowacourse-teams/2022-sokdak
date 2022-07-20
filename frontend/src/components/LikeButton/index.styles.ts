6cf09d38b7d3fa5e3ef0d67f420b1ad804499c38
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-07-20 22:28:06 +0900

    fix: 좋아요 수에 따라 하트 svg 크기가 달라지지 않도록 수정

--- frontend/src/components/LikeButton/index.styles.ts
+++ frontend/src/components/LikeButton/index.styles.ts
@@ -4,11 +4,12 @@ export const Button = styled.button<{ isLiked: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 60px;
+  min-width: 50px;
   height: 30px;
   border-radius: 15px;
   border: 1px solid ${props => (props.isLiked ? props.theme.colors.pink_300 : props.theme.colors.gray_300)};
   gap: 4px;
-  color: ${props => (props.isLiked ? props.theme.colors.pink_300 : props.theme.colors.gray_300)};
   background-color: inherit;
+  font-size: 13px;
+  color: ${props => (props.isLiked ? props.theme.colors.pink_300 : props.theme.colors.gray_300)};
 `;

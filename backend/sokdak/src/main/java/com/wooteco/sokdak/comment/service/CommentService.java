dfcaf2fa89e04cebcef098bc69412cca4652fb0c
injoon2019 <ison@sfu.ca>
2022-07-21 22:38:51 +0900

    refactor: CommentService 트랜잭션 어노테이션 선언
    - readOnly 클래스 단위

--- backend/sokdak/src/main/java/com/wooteco/sokdak/comment/service/CommentService.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/comment/service/CommentService.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class CommentService {
 
     private final CommentRepository commentRepository;

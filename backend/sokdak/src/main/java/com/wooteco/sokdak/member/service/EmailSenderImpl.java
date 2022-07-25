6ca3289a9eed9ea067bfea5185b9f351c1284d30
injoon2019 <ison@sfu.ca>
2022-07-25 19:57:48 +0900

    feat: 이메일 Config 삭제
    - 스프링 EmailSender를 사용하면 설정파일을 읽어서 MailProperties 주입 가능

--- backend/sokdak/src/main/java/com/wooteco/sokdak/member/service/EmailSenderImpl.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/member/service/EmailSenderImpl.java
@@ -1,5 +1,6 @@
 package com.wooteco.sokdak.member.service;
 
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -7,18 +8,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailSenderImpl implements EmailSender {
 
-    public static final String FROM = "sokdakX2@gmail.com";
-
+    private final MailProperties properties;
     private final JavaMailSender javaMailSender;
 
-    public EmailSenderImpl(JavaMailSender javaMailSender) {
+    public EmailSenderImpl(MailProperties properties, JavaMailSender javaMailSender) {
+        this.properties = properties;
         this.javaMailSender = javaMailSender;
     }
 
     @Override
     public void send(String email, String authCode) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(FROM);
+        message.setFrom(properties.getUsername());
         message.setTo(email);
         message.setSubject("속닥속닥 인증코드입니다.");
         message.setText(authCode);

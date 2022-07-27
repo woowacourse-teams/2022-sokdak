47313fdd1c6b47aefce7f206ac3fa06e5d3b81cb
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-07-27 14:10:18 +0900

    refactor: 사용되지 않는 type 삭제

--- frontend/src/components/PostListItem/index.tsx
+++ frontend/src/components/PostListItem/index.tsx
@@ -5,7 +5,7 @@ import * as Styled from './index.styles';
 import countFormatter from '@/utils/countFormatter';
 import timeConverter from '@/utils/timeConverter';
 
-interface PostListItemProps extends Omit<Post, 'id' | 'like' | 'hashtags'> {
+interface PostListItemProps extends Omit<Post, 'id' | 'like' | 'hashtags' | 'authorized'> {
   handleClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 

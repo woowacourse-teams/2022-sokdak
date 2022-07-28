81b1f7a68c4f13c808763378ac5719a33bda78aa
= <rerub0831@gmail.com>
2022-07-28 15:16:00 +0900

    test: 해시태그 목록 조회 인수테스트 작성

--- backend/sokdak/src/main/java/com/wooteco/sokdak/post/domain/Post.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/post/domain/Post.java
@@ -1,6 +1,7 @@
 package com.wooteco.sokdak.post.domain;
 
 import com.wooteco.sokdak.auth.exception.AuthenticationException;
+import com.wooteco.sokdak.board.domain.PostBoard;
 import com.wooteco.sokdak.comment.domain.Comment;
 import com.wooteco.sokdak.hashtag.domain.PostHashtag;
 import com.wooteco.sokdak.like.domain.Like;

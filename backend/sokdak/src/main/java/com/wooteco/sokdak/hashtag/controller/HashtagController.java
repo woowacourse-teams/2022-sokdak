b62b4bdb321b0e61707e1ed878e4df5921a79921
= <rerub0831@gmail.com>
2022-07-28 19:36:35 +0900

    feat: 해시태그 목록 조회 기능 구현

--- backend/sokdak/src/main/java/com/wooteco/sokdak/hashtag/controller/HashtagController.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/hashtag/controller/HashtagController.java
@@ -2,10 +2,12 @@ package com.wooteco.sokdak.hashtag.controller;
 
 import static org.springframework.data.domain.Sort.Direction.DESC;
 
+import com.wooteco.sokdak.hashtag.dto.HashtagsSearchResponse;
 import com.wooteco.sokdak.hashtag.service.HashtagService;
 import com.wooteco.sokdak.post.dto.PostsResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,11 @@ public class HashtagController {
                                                               @PageableDefault(sort = "createdAt", direction = DESC) Pageable pageable) {
         PostsResponse postsResponse = hashtagService.findPostsWithHashtag(hashtag, pageable);
         return ResponseEntity.ok(postsResponse);
+    }
+
+    @GetMapping(path = "/hashtags/popular", params = {"limit", "include"})
+    public ResponseEntity<HashtagsSearchResponse> findHashtagsWithTagName(@RequestParam String include, @RequestParam int limit) {
+        HashtagsSearchResponse hashtagsSearchResponse = hashtagService.findHashtagsWithTagName(include,limit);
+        return ResponseEntity.status(HttpStatus.OK).body(hashtagsSearchResponse);
     }
 }

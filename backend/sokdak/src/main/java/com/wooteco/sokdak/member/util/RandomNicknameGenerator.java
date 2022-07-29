2e682db4fdb6caa3cf12bc9459584c357e5c6717
injoon2019 <ison@sfu.ca>
2022-07-29 10:51:04 +0900

    feat: 랜덤닉네임 순서 랜덤화

--- backend/sokdak/src/main/java/com/wooteco/sokdak/member/util/RandomNicknameGenerator.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/member/util/RandomNicknameGenerator.java
@@ -1,11 +1,15 @@
 package com.wooteco.sokdak.member.util;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RandomNicknameGenerator {
 
-    private static final String[] ADJECTIVES = {"행복한", "즐거운", "슬픈", "화난", "기쁜", "짜증난", "울적한", "신기한", "재밌는", "속상한",
+    private static final Random RANDOM = new Random();
+    private static final String[] ADJECTIVES = {"행복한", "즐거운", "슬픈", "화난", "기쁜", "울적한", "신기한", "재밌는", "속상한", "기쁜", "배고픈",
             "걱정많은"};
     private static final String[] NOUNS = {"404", "200", "자스", "타스", "리액트", "스프링", "자바", "파이썬", "노드", "AWS", "맥북",
             "윈도우", "마우스", "키보드", "노드", "도커"};
@@ -20,12 +24,13 @@ public class RandomNicknameGenerator {
     }
 
     public static String generate(Set<String> usedNicknames) {
-        return RANDOM_NICKNAMES.stream()
+        List<String> usableNicknames = RANDOM_NICKNAMES.stream()
                 .filter(nickname -> !usedNicknames.contains(nickname))
-                .findFirst()
-                .orElse("크루");
-    }
+                .collect(Collectors.toList());
 
-    private RandomNicknameGenerator() {
+        if (usableNicknames.size() > 0) {
+            return usableNicknames.get(RANDOM.nextInt(usableNicknames.size()));
+        }
+        return "크루";
     }
 }

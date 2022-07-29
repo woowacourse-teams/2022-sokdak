e61aa700a009b5bf0a8c4131bd35919c0682509f
JUDONGHYEOK <judonghyeok@gmail.com>
2022-07-29 09:55:08 +0900

    test: 회원가입 validation 테스트 추가 및 성공 로직 수정

--- frontend/src/__test__/Member.test.tsx
+++ frontend/src/__test__/Member.test.tsx
@@ -23,6 +23,47 @@ describe('멤버 테스트', () => {
     fireEvent.click(submitButton!);
   }
 
+  async function registerEmail(email: string) {
+    const emailInput = await screen.findByPlaceholderText('이메일');
+
+    fireEvent.change(emailInput, { target: { value: email } });
+    fireEvent.click(await screen.findByText('인증번호 받기'));
+  }
+
+  async function registerVerificationCode(code: string) {
+    const verificationCodeInput = await screen.findByPlaceholderText('인증번호');
+
+    fireEvent.change(verificationCodeInput, { target: { value: code } });
+    fireEvent.click(await screen.findByText('확인'));
+  }
+
+  async function registerID(ID: string) {
+    const IDInput = await screen.findByPlaceholderText('아이디');
+    const [IDsubmitButton] = await screen.findAllByText('중복 확인');
+
+    fireEvent.change(IDInput, { target: { value: ID } });
+    fireEvent.click(IDsubmitButton);
+    await waitFor(() => {});
+  }
+
+  async function registerNickname(nickname: string) {
+    const [_, nicknameSubmitButton] = await screen.findAllByText('중복 확인');
+    const nickNameInput = await screen.findByPlaceholderText('닉네임');
+
+    fireEvent.change(nickNameInput, { target: { value: nickname } });
+    fireEvent.click(nicknameSubmitButton);
+  }
+
+  async function registerPassword(password: string) {
+    const passwordInput = await screen.findByPlaceholderText('비밀번호');
+    fireEvent.change(passwordInput, { target: { value: password } });
+  }
+
+  async function registerPasswordConfirmation(password: string) {
+    const passwordConfirmationInput = await screen.findByPlaceholderText('비밀번호 확인');
+    fireEvent.change(passwordConfirmationInput, { target: { value: password } });
+  }
+
   beforeEach(() => {
     const queryClient = new QueryClient();
     const snackBarElement = document.createElement('div');
@@ -50,19 +91,19 @@ describe('멤버 테스트', () => {
       fireEvent.click(await screen.findByText('로그인'));
     });
 
-    test('올바른 이메일과 비밀번호를 입력할 경우 로그인에 성공한다.', () => {
+    test('올바른 이메일과 비밀번호를 입력할 경우 로그인에 성공한다.', async () => {
       attemptLogin(memberList[0].username, memberList[0].password);
 
-      waitFor(() => {
-        expect(screen.findByText(SNACKBAR_MESSAGE.SUCCESS_LOGIN)).toBeInTheDocument();
+      await waitFor(async () => {
+        expect(await screen.findByText(SNACKBAR_MESSAGE.SUCCESS_LOGIN)).toBeInTheDocument();
       });
     });
 
-    test('올바르지 않은 이메일과 비밀번호를 입력할 경우 로그인에 성공한다.', () => {
+    test('올바르지 않은 이메일과 비밀번호를 입력할 경우 로그인에 실패한다.', async () => {
       attemptLogin('InvalidID', memberList[0].password);
 
-      waitFor(() => {
-        expect(screen.findByText(SNACKBAR_MESSAGE.FAIL_LOGIN)).toBeInTheDocument();
+      await waitFor(async () => {
+        expect(await screen.findByText(SNACKBAR_MESSAGE.FAIL_LOGIN)).toBeInTheDocument();
       });
     });
   });
@@ -73,38 +114,71 @@ describe('멤버 테스트', () => {
       fireEvent.click(await screen.findByText('간편 회원가입하기'));
     });
 
+    const validMemberForm = validMemberEmail.find(member => !member.isSignedUp)!;
+    const existedUser = validMemberEmail.find(member => member.isSignedUp)!;
+
     test('올바른 양식을 입력하면 회원가입에 성공한다.', async () => {
-      const targetMemberForm = validMemberEmail.find(member => !member.isSignedUp)!;
+      registerEmail(validMemberForm.email);
 
-      const emailInput = await screen.findByPlaceholderText('이메일');
-      fireEvent.change(emailInput, { target: { value: targetMemberForm.email } });
-      fireEvent.click(await screen.findByText('인증번호 받기'));
-
-      const verificationCodeInput = await screen.findByPlaceholderText('인증번호');
-      fireEvent.change(verificationCodeInput, { target: { value: targetMemberForm.code } });
-      fireEvent.click(await screen.findByText('확인'));
-
-      const IDInput = await screen.findByPlaceholderText('아이디');
-      const [IDsubmitButton, nicknameSubmitButton] = await screen.findAllByText('중복 확인');
-      fireEvent.change(IDInput, { target: { value: 'validID' } });
-      fireEvent.click(IDsubmitButton);
-
-      const nickNameInput = await screen.findByPlaceholderText('닉네임');
-      fireEvent.change(nickNameInput, { target: { value: 'validNickname' } });
-      fireEvent.change(nicknameSubmitButton);
+      await waitFor(async () => {
+        await registerVerificationCode(validMemberForm.code);
+        await registerID('validID');
+        await registerNickname('validNickname');
+      });
+      await screen.findAllByText('중복 확인 완료');
 
       const validPassword = 'validPassword123!';
-      const passwordInput = await screen.findByPlaceholderText('비밀번호');
-      fireEvent.change(passwordInput, { target: { value: validPassword } });
+      registerPassword(validPassword);
+      registerPasswordConfirmation(validPassword);
 
-      const passwordConfirmationInput = await screen.findByPlaceholderText('비밀번호 확인');
-      fireEvent.change(passwordConfirmationInput, { target: { value: validPassword } });
+      await waitFor(async () => {
+        const signUpButton = (await screen.findAllByText('회원가입')).find(button => button.tagName === 'BUTTON');
+        fireEvent.click(signUpButton!);
+      });
 
-      const signUpButton = await (await screen.findAllByText('회원가입')).find(button => button.tagName === 'BUTTON');
-      fireEvent.click(signUpButton!);
+      await waitFor(async () => {
+        expect(await screen.findByText(SNACKBAR_MESSAGE.SUCCESS_SIGN_UP)).toBeInTheDocument();
+      });
+    });
 
-      waitFor(() => {
-        expect(screen.getByText(SNACKBAR_MESSAGE.SUCCESS_SIGN_UP)).toBeInTheDocument();
+    test('우아한 테크코스 크루가 아닐경우 해당 에러 메시지를 표시한다.', async () => {
+      registerEmail('notIncludedEmail@gmail.com');
+
+      await waitFor(() => {
+        expect(screen.getByText('우아한테크코스 크루가 아닙니다.')).toBeInTheDocument();
+      });
+    });
+
+    test('이미 가입된 크루일 경우 해당 에러 메시지를 표시한다.', async () => {
+      registerEmail(existedUser?.email!);
+
+      await waitFor(() => {
+        expect(screen.getByText('이미 가입된 크루입니다.')).toBeInTheDocument();
+      });
+    });
+
+    test('인증번호가 올바르지 않을 경우 해당 에러 메시지를 표시한다.', async () => {
+      registerEmail(validMemberForm.email);
+      registerVerificationCode('IncorrectCode');
+
+      await waitFor(() => {
+        expect(screen.getByText('잘못된 인증번호입니다.')).toBeInTheDocument();
+      });
+    });
+
+    test('이미 존재하는 아이디를 입력한 경우 해당 에러 메시지를 표시한다.', async () => {
+      registerID(existedUser?.ID!);
+
+      await waitFor(() => {
+        expect(screen.getByText('중복된 아이디입니다.')).toBeInTheDocument();
+      });
+    });
+
+    test('이미 존재하는 닉네임을 입력한 경우 해당 에러 메시지를 표시한다.', async () => {
+      registerNickname(existedUser?.nickname!);
+
+      await waitFor(() => {
+        expect(screen.getByText('중복된 닉네임입니다.')).toBeInTheDocument();
       });
     });
   });

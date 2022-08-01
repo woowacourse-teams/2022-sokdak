d5f9e39de2ddacb08a6342794160c88d1565e990
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-08-01 22:25:38 +0900

    refactor: 젠킨스 파일 수정

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -14,6 +14,7 @@ const config: Config.InitialOptions = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  roots: ['<rootDir>/__tests__'],
 };
 
 export default config;

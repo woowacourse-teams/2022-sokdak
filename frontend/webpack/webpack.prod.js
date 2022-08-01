4270da10712a4be873f7c65907df595bb3bf79ea
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-08-01 21:56:36 +0900

    refactor: 젠킨스 파일 수정

--- frontend/webpack/webpack.prod.js
+++ frontend/webpack/webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new DefinePlugin({
-      'process.env.API_URL': JSON.stringify('http://192.168.1.241'),
+      'process.env.API_URL': JSON.stringify('https://192.168.1.241'),
       'process.env.MODE': JSON.stringify('PRODUCTION'),
     }),
   ],

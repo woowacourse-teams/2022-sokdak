ea98182e0e17af0e19130798a77f86adc9aeccf0
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-08-02 17:13:35 +0900

    refactor: 변경될 일 없는 변수를 상수로 선언

--- frontend/src/components/SearchModal/index.tsx
+++ frontend/src/components/SearchModal/index.tsx
@@ -15,7 +15,7 @@ interface SearchModalProps {
 
 const SearchModal = ({ handleSearchModal: closeModal }: SearchModalProps) => {
   const navigate = useNavigate();
-  const [limit] = useState(10);
+  const limit = 10;
   const [include, setInclude] = useState('');
   const { debounceValue: debouncedInclude } = useQueryDebounce(include);
   useModalHistory({ closeModal });

b7bc83a0bf9782c46f55df872c349b2a71e3cf67
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-08-04 14:46:58 +0900

    refactor: 쓸모없는 state 제거

--- frontend/src/components/Timer/index.tsx
+++ frontend/src/components/Timer/index.tsx
@@ -10,17 +10,11 @@ interface TimerProps {
 
 const Timer = ({ className, startMinute, callback }: TimerProps) => {
   const [totalSecond, setTotalSecond] = useState(startMinute * 60);
-  const [minute, setMinute] = useState(startMinute);
-  const [second, setSecond] = useState(0);
 
   useEffect(() => {
-    const countdown = setInterval(() => {
-      setTotalSecond(state => state - 1);
-      setMinute(Math.floor(totalSecond / 60));
-      setSecond(totalSecond % 60);
-    }, 1000);
+    const countdown = setInterval(() => setTotalSecond(second => second - 1), 1000);
 
-    if (totalSecond < 0) {
+    if (totalSecond <= 0) {
       clearInterval(countdown);
       callback();
     }
@@ -30,7 +24,7 @@ const Timer = ({ className, startMinute, callback }: TimerProps) => {
 
   return (
     <Styled.Container className={className}>
-      {minute} : {second.toString().padStart(2, '0')}
+      {Math.floor(totalSecond / 60)} : {(totalSecond % 60).toString().padStart(2, '0')}
     </Styled.Container>
   );
 };

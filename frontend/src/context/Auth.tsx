bc616b171af293a8a1fbd009126a0cb58aaefd57
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2022-08-04 15:53:49 +0900

    refactor: locatStorgae.removeItem
    Co-authored-by: YoungHwa Byun <rlaalsdl111@gmail.com>

--- frontend/src/context/Auth.tsx
+++ frontend/src/context/Auth.tsx
@@ -20,7 +20,7 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
   useEffect(() => {
     const refreshToken = localStorage.getItem(STORAGE_KEY.REFRESH_TOKEN);
     if (refreshToken && isExpired(parseJwt(refreshToken)!)) {
-      localStorage.setItem(STORAGE_KEY.ACCESS_TOKEN, '');
+      localStorage.removeItem(STORAGE_KEY.ACCESS_TOKEN);
       localStorage.setItem(STORAGE_KEY.REFRESH_TOKEN, '');
       return;
     }

d863ac8c9865498df437b8829da8b15ccad52a35
injoon2019 <ison@sfu.ca>
2022-08-04 22:59:41 +0900

    feat: 관리자가 티켓 등록 기능 구현

---
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/admin/controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.wooteco.sokdak.admin.controller;
+
+import com.wooteco.sokdak.admin.dto.EmailsAddRequest;
+import com.wooteco.sokdak.admin.service.AdminService;
+import com.wooteco.sokdak.auth.dto.AuthInfo;
+import com.wooteco.sokdak.support.token.Login;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> success() {
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("/email")
+    public ResponseEntity<Void> registerEmail(@Login AuthInfo authInfo, @RequestBody EmailsAddRequest emailsAddRequest) {
+        adminService.registerEmail(authInfo, emailsAddRequest);
+
+        return ResponseEntity.noContent().build();
+    }
+}

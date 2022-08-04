6bf27537cffb7e4a9dbee3d69885e190b381559e
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-08-04 18:36:19 +0900

    style: 해시태그 안 글씨 정렬 (#345)

--- frontend/src/components/HashTag/index.styles.ts
+++ frontend/src/components/HashTag/index.styles.ts
@@ -2,19 +2,24 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div<{ onClick: React.MouseEventHandler<HTMLDivElement> | undefined }>`
   min-width: fit-content;
-  height: 23px;
+  height: fit-content;
   border: 1px solid ${props => props.theme.colors.sub};
   border-radius: 15px;
   background-color: white;
   font-size: 13px;
   color: ${props => props.theme.colors.sub};
-  padding: 6px;
+  padding: 6px 8px 5px 8px;
   box-sizing: border-box;
   user-select: none;
   cursor: ${props => (props.onClick ? 'pointer' : null)};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Count = styled.span`
   color: ${props => props.theme.colors.gray_200};
   font-size: 11px;
+  margin-left: 3px;
 `;

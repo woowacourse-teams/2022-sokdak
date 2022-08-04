cdf46f4f98627e8dc2e3c5d7049d73b3df4268dd
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-08-04 14:37:43 +0900

    fix: 현재 기능과 맞지 않는 테스트 제거

--- frontend/src/__test__/Post.test.tsx
+++ frontend/src/__test__/Post.test.tsx
@@ -4,11 +4,8 @@ import * as ReactRouter from 'react-router-dom';
 
 import PostPage from '@/pages/PostPage';
 
-import { AuthContextProvider } from '@/context/Auth';
-import { SnackBarContextProvider } from '@/context/Snackbar';
-
 import App from '@/App';
-import { memberList, postList } from '@/dummy';
+import { postList } from '@/dummy';
 import theme from '@/style/theme';
 import { ThemeProvider } from '@emotion/react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
@@ -80,71 +77,14 @@ describe('게시글 확인 테스트', () => {
     );
   });
 
-  test('글 목록에서 특정 글을 클릭하면 해당 글 정보를 볼 수 있다.', async () => {
+  test('메인 페이지에서 특정 글을 클릭하면 해당 글 정보를 볼 수 있다.', async () => {
     fireEvent.click(await screen.findByTestId(postId));
 
     const targetPost = postList.find(post => postId === post.id)!;
 
-    expect(await screen.findByText(targetPost.title)).toBeInTheDocument();
-    expect(await screen.findByText(targetPost.content)).toBeInTheDocument();
-  });
-});
-
-describe('게시글 작성 테스트', () => {
-  beforeEach(async () => {
-    const queryClient = new QueryClient();
-    const snackBarElement = document.createElement('div');
-    snackBarElement.id = 'snackbar';
-
-    render(
-      <SnackBarContextProvider>
-        <AuthContextProvider>
-          <QueryClientProvider client={queryClient}>
-            <MemoryRouter initialEntries={[`/`]}>
-              <ThemeProvider theme={theme}>
-                <App />
-              </ThemeProvider>
-            </MemoryRouter>
-          </QueryClientProvider>
-        </AuthContextProvider>
-      </SnackBarContextProvider>,
-    );
-
-    if (!document.getElementById('snackbar')) document.body.appendChild(snackBarElement);
-
-    async function login() {
-      fireEvent.click(await screen.findByText('로그인'));
-
-      const IDInput = await screen.findByPlaceholderText('아이디');
-      const passwordInput = await screen.findByPlaceholderText('비밀번호');
-      const submitButton = (await screen.findAllByText('로그인')).find(button => button.tagName === 'BUTTON');
-      const [targetMember] = memberList;
-
-      fireEvent.change(IDInput, { target: { value: targetMember.username } });
-      fireEvent.change(passwordInput, { target: { value: targetMember.password } });
-      fireEvent.click(submitButton!);
-
-      expect(await screen.findByText(targetMember.username[0])).toBeInTheDocument();
-      expect(screen.queryByText('로그인')).toBe(null);
-    }
-
-    await login();
-  });
-
-  test('로그인한 사용자는 글을 작성할 수 있다.', async () => {
-    fireEvent.click(await screen.findByText('+'));
-
-    const titleInput = await screen.findByPlaceholderText('제목을 입력해주세요.');
-    const contentInput = await screen.findByPlaceholderText('내용을 작성해주세요.');
-    const tagInput = await screen.findByPlaceholderText('태그를 입력해주세요.');
-
-    fireEvent.change(titleInput, { target: { value: '입력된 제목' } });
-    fireEvent.change(contentInput, { target: { value: '입력된 내용' } });
-    fireEvent.change(tagInput, { target: { value: '새로운 태그,' } });
-    fireEvent.click(await screen.findByText('글 작성하기'));
-
-    expect(await screen.findByTestId(postList.length + 1)).toBeInTheDocument();
-    expect(await screen.findByText('입력된 제목')).toBeInTheDocument();
-    expect(await screen.findByText('입력된 내용')).toBeInTheDocument();
+    waitFor(async () => {
+      expect(await screen.findByText(targetPost.title)).toBeInTheDocument();
+      expect(await screen.findByText(targetPost.content)).toBeInTheDocument();
+    });
   });
 });

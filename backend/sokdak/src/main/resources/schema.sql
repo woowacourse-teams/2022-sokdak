9fffb337964d5e69b23581f09569f203ef218f4b
hyunrrr <whgusrms96@gmail.com>
2022-08-05 10:07:05 +0900

    chore: 티켓만 초기 데이터로 넣도록 수정

--- backend/sokdak/src/main/resources/schema.sql
+++ backend/sokdak/src/main/resources/schema.sql
@@ -117,13 +117,13 @@ create table if not exists ticket
     primary key (ticket_id)
     );
 
-insert into member (username, nickname, password, role_type) values ('chris', 'chris', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
-insert into member (username, nickname, password, role_type) values ('east', 'east', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
-insert into member (username, nickname, password, role_type) values ('hunch', 'hunch', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
-insert into member (username, nickname, password, role_type) values ('josh', 'josh', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
-insert into member (username, nickname, password, role_type) values ('dongkey', 'dongkey', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
-insert into member (username, nickname, password, role_type) values ('movie', 'movie', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
-insert into member (username, nickname, password, role_type) values ('thor', 'thor', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
+-- insert into member (username, nickname, password, role_type) values ('chris', 'chris', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
+-- insert into member (username, nickname, password, role_type) values ('east', 'east', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
+-- insert into member (username, nickname, password, role_type) values ('hunch', 'hunch', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
+-- insert into member (username, nickname, password, role_type) values ('josh', 'josh', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
+-- insert into member (username, nickname, password, role_type) values ('dongkey', 'dongkey', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
+-- insert into member (username, nickname, password, role_type) values ('movie', 'movie', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
+-- insert into member (username, nickname, password, role_type) values ('thor', 'thor', '6297d64078fc9abcfe37d0e2c910d4798bb4c04502d7dd1207f558860c2b382e', 'ADMIN');
 
 insert into ticket (serial_number, used) values ('21f46568bf6002c23843d198af30bb2bc8123695bd3d12ce86e0fc35bc5d3279', false);
 insert into ticket (serial_number, used) values ('5810c81b6f78b1bcf62b53035d879e1309750ab60d1b4b601dfbc368005645cb', false);
@@ -133,7 +133,7 @@ insert into ticket (serial_number, used) values ('774dd927c9c846f2bb21b02fd139d2
 insert into ticket (serial_number, used) values ('ce89c8413662dffc17a4644ddf0386432404cd943b18eeee45740be5c35ef03b', false);
 insert into ticket (serial_number, used) values ('49d3b5b2d51e0f03cd1c0b85e2312dbd740023856ce16a725a3617f58b91da1c', false);
 
-insert into board (title, board_type) values ('Hot 게시판', 'NON_WRITABLE');
-insert into board (title, board_type) values ('자유게시판', 'WRITABLE');
-insert into board (title, board_type) values ('포수타', 'WRITABLE');
-insert into board (title, board_type) values ('감동크루', 'WRITABLE');
+-- insert into board (title, board_type) values ('Hot 게시판', 'NON_WRITABLE');
+-- insert into board (title, board_type) values ('자유게시판', 'WRITABLE');
+-- insert into board (title, board_type) values ('포수타', 'WRITABLE');
+-- insert into board (title, board_type) values ('감동크루', 'WRITABLE');

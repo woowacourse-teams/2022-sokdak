3e03c916c7efb8783d2ef8ea21ea631f5581d804
hyunrrr <whgusrms96@gmail.com>
2022-08-09 15:51:05 +0900

    feat: Password 값객체 추가

---
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/member/domain/Password.java
@@ -0,0 +1,33 @@
+package com.wooteco.sokdak.member.domain;
+
+import com.wooteco.sokdak.auth.service.Encryptor;
+import com.wooteco.sokdak.member.exception.InvalidUsernameException;
+import java.util.regex.Pattern;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class Password {
+
+    private static final Pattern PATTERN =
+            Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$");
+
+    @Column(name = "password")
+    private String value;
+
+    protected Password() {
+    }
+
+    public Password(String value) {
+        validate(value);
+        this.value = Encryptor.encrypt(value);
+    }
+
+    private void validate(String value) {
+        if (!PATTERN.matcher(value).matches()) {
+            throw new InvalidUsernameException();
+        }
+    }
+}

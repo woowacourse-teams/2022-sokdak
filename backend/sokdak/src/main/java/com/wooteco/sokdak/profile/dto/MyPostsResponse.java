068d8d6f2a27e68c26fe13f312f221c60701d605
= <rerub0831@gmail.com>
2022-08-09 20:42:43 +0900

    refactor: 변환용 정적 팩토리 메소드 생성

--- backend/sokdak/src/main/java/com/wooteco/sokdak/profile/dto/MyPostsResponse.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/profile/dto/MyPostsResponse.java
@@ -1,7 +1,9 @@
 package com.wooteco.sokdak.profile.dto;
 
+import com.wooteco.sokdak.post.domain.Post;
 import com.wooteco.sokdak.post.dto.PostsElementResponse;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 
 @Getter
@@ -16,5 +18,12 @@ public class MyPostsResponse {
     public MyPostsResponse(List<PostsElementResponse> posts, int totalPageCount) {
         this.posts = posts;
         this.totalPageCount = totalPageCount;
+    }
+
+    public static MyPostsResponse ofPosts(List<Post> posts, int totalPageCount) {
+        List<PostsElementResponse> postsElementResponses = posts.stream()
+                .map(PostsElementResponse::from)
+                .collect(Collectors.toList());
+        return new MyPostsResponse(postsElementResponses, totalPageCount);
     }
 }

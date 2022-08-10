5e38c44ac3ed04d8c0305e3e7a7c3674a0f49122
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-08-10 22:45:27 +0900

    feat: 새로 추가한 element 기준으로 스크롤

--- frontend/src/utils/scrollToCurrent.ts
+++ frontend/src/utils/scrollToCurrent.ts
@@ -1,8 +1,12 @@
 const scrollToCurrent = (current: number) => {
+  document.body.style.height = document.body.scrollHeight + current + 'px';
+
   window.scrollTo({
     top: document.documentElement.scrollTop + current,
     behavior: 'smooth',
   });
+
+  document.body.style.height = document.body.scrollHeight - current + 'px';
 };
 
 export default scrollToCurrent;

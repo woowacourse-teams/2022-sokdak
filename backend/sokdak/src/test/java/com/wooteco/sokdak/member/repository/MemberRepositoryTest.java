a41d173586e7bfb030bd9e33f7a82567d2fad15f
Chris <bjuuuu98@gmail.com>
2022-08-16 14:51:43 +0900

    refactor: Repository 레이어 테스트 픽스쳐 분리

--- backend/sokdak/src/test/java/com/wooteco/sokdak/member/repository/MemberRepositoryTest.java
+++ backend/sokdak/src/test/java/com/wooteco/sokdak/member/repository/MemberRepositoryTest.java
@@ -1,23 +1,16 @@
 package com.wooteco.sokdak.member.repository;
 
-import static com.wooteco.sokdak.util.fixture.MemberFixture.VALID_NICKNAME;
-import static com.wooteco.sokdak.util.fixture.MemberFixture.VALID_PASSWORD;
-import static com.wooteco.sokdak.util.fixture.MemberFixture.VALID_USERNAME;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import com.wooteco.sokdak.config.JPAConfig;
 import com.wooteco.sokdak.member.domain.Member;
+import com.wooteco.sokdak.util.RepositoryTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.dao.DataIntegrityViolationException;
 
-@DataJpaTest
-@Import(JPAConfig.class)
-class MemberRepositoryTest {
+class MemberRepositoryTest extends RepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
@@ -25,33 +18,20 @@ class MemberRepositoryTest {
     @DisplayName("회원 id에 해당하는 닉네임 값 반환")
     @Test
     void findNicknameValueById() {
-        Member member = Member.builder()
-                .username(VALID_USERNAME)
-                .password(VALID_PASSWORD)
-                .nickname(VALID_NICKNAME)
-                .build();
-        memberRepository.save(member);
-
-        String actual = memberRepository.findNicknameValueById(member.getId())
+        String actual = memberRepository.findNicknameValueById(member1.getId())
                 .get();
 
-        assertThat(actual).isEqualTo(VALID_NICKNAME);
+        assertThat(actual).isEqualTo("chrisNickname");
     }
 
     @DisplayName("중복된 닉네임으로 회원가입할 수 없다.")
     @Test
     void prevent_Duplicated_Nickname() {
-        Member member = Member.builder()
-                .username(VALID_USERNAME)
-                .password(VALID_PASSWORD)
-                .nickname("joshNickname")
-                .build();
         Member duplicatedNicknameMember = Member.builder()
                 .username("josh")
                 .password("Abcd123!@")
-                .nickname("joshNickname")
+                .nickname("chrisNickname")
                 .build();
-        memberRepository.save(member);
 
         assertThatThrownBy(() -> memberRepository.save(duplicatedNicknameMember))
                 .isInstanceOf(DataIntegrityViolationException.class);
--- backend/sokdak/src/test/java/com/wooteco/sokdak/util/RepositoryTest.java
+++ backend/sokdak/src/test/java/com/wooteco/sokdak/util/RepositoryTest.java
@@ -1,33 +1,35 @@
 package com.wooteco.sokdak.util;
 
+import com.wooteco.sokdak.config.JPAConfig;
 import com.wooteco.sokdak.member.domain.Member;
 import com.wooteco.sokdak.member.repository.MemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest
+@Import(JPAConfig.class)
 public class RepositoryTest {
 
     @Autowired
-    public MemberRepository memberRepository;
+    protected MemberRepository memberRepository;
 
-    public Member member;
+    protected Member member1;
 
     @BeforeEach
     void inputData() {
-        member = Member.builder()
+        member1 = Member.builder()
                 .username("chris")
                 .password("Abcd123!@")
                 .nickname("chrisNickname")
                 .build();
-        memberRepository.save(member);
+        memberRepository.save(member1);
     }
 
     @AfterEach
     void cleanUp() {
-        memberRepository.delete(member);
+        memberRepository.delete(member1);
     }
-
 }

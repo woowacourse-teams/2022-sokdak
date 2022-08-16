dc3aced7e7fe34d36dac6bcd7ff84754d3b45a1d
hunch <rerub0831@gmail.com>
2022-08-16 16:16:38 +0900

    feat: admin api 구현 #473
    * feat: 어드민 api 및 컨트롤러/서비스 테스트 구현
    
    * feat: 어드민 api 구현
    
    * feat: 어드민 api 인수테스트 구현
    
    * test: 어드민 api 컨트롤러 테스트 추가 및 restdocs 작성
    
    * style: 일부 메소드명 대문자 및 컨벤션에 맞게 코드 변경
    
    * fix: 관리자 권한 관련 예외 상태코드 Forbidden으로 수정 및 빈 생성자 추가
    
    * refactor: 관리자 api 2차 리뷰 반영 리팩토링

--- backend/sokdak/src/main/java/com/wooteco/sokdak/admin/controller/AdminController.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/admin/controller/AdminController.java
@@ -1,11 +1,17 @@
 package com.wooteco.sokdak.admin.controller;
 
 import com.wooteco.sokdak.admin.dto.EmailsAddRequest;
+import com.wooteco.sokdak.admin.dto.PostReportsResponse;
+import com.wooteco.sokdak.admin.dto.TicketRequest;
+import com.wooteco.sokdak.admin.dto.TicketsResponse;
 import com.wooteco.sokdak.admin.service.AdminService;
 import com.wooteco.sokdak.auth.dto.AuthInfo;
 import com.wooteco.sokdak.support.token.Login;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +32,54 @@ public class AdminController {
         return ResponseEntity.ok().build();
     }
 
-
     @PostMapping("/email")
-    public ResponseEntity<Void> registerEmail(@Login AuthInfo authInfo, @RequestBody EmailsAddRequest emailsAddRequest) {
+    public ResponseEntity<Void> registerEmail(@Login AuthInfo authInfo,
+                                              @RequestBody EmailsAddRequest emailsAddRequest) {
         adminService.registerEmail(authInfo, emailsAddRequest);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id, @Login AuthInfo authInfo) {
+        adminService.deletePost(id, authInfo);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/posts/{id}/postreports/{postReportCount}")
+    public ResponseEntity<Void> blockPost(@PathVariable Long id, @PathVariable Long postReportCount,
+                                          @Login AuthInfo authInfo) {
+        adminService.blockPost(id, postReportCount, authInfo);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/posts/{id}/postreports")
+    public ResponseEntity<Void> unblockPost(@PathVariable Long id, @Login AuthInfo authInfo) {
+        adminService.unblockPost(id, authInfo);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/postreports")
+    public ResponseEntity<PostReportsResponse> findAllPostReports(@Login AuthInfo authInfo) {
+        PostReportsResponse postReportsResponse = adminService.findAllPostReports(authInfo);
+        return ResponseEntity.ok().body(postReportsResponse);
+    }
+
+    @GetMapping("/tickets")
+    public ResponseEntity<TicketsResponse> findAllTickets(@Login AuthInfo authInfo) {
+        TicketsResponse allTicket = adminService.findAllTickets(authInfo);
+        return ResponseEntity.ok().body(allTicket);
+    }
+
+    @PostMapping("/tickets")
+    public ResponseEntity<Void> saveTicket(@Login AuthInfo authInfo, @RequestBody TicketRequest ticketRequest) {
+        adminService.saveTicket(authInfo, ticketRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/tickets")
+    public ResponseEntity<Void> updateTicket(@Login AuthInfo authInfo, @RequestBody TicketRequest ticketRequest) {
+        adminService.changeTicketUsedState(authInfo, ticketRequest);
+        return ResponseEntity.ok().build();
     }
 }
---
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/admin/dto/TicketRequest.java
@@ -0,0 +1,38 @@
+package com.wooteco.sokdak.admin.dto;
+
+import com.wooteco.sokdak.auth.domain.Ticket;
+import javax.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class TicketRequest {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    private String serialNumber;
+    @NotBlank(message = "사용 여부를 입력해주세요.")
+    private boolean used;
+
+    protected TicketRequest(){
+    }
+
+    @Builder
+    public TicketRequest(String serialNumber, boolean used) {
+        this.serialNumber = serialNumber;
+        this.used = used;
+    }
+
+    public static TicketRequest of(Ticket ticket) {
+        return TicketRequest.builder()
+                .serialNumber(ticket.getSerialNumber())
+                .used(ticket.isUsed())
+                .build();
+    }
+
+    public Ticket toTicket(){
+        return Ticket.builder()
+                .serialNumber(serialNumber)
+                .used(used)
+                .build();
+    }
+}

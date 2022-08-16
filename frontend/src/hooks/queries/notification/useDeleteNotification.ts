d8d7368c0d0d8fe5b8ab8b6b2ae6c844adf4eafb
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2022-08-16 10:48:01 +0900

    fix: 알람 delete시 데이터 동기화 문제 수정 (#471)
    - 알람 delete시 setqueriesData -> refetchQueries로 변경

--- frontend/src/hooks/queries/notification/useDeleteNotification.ts
+++ frontend/src/hooks/queries/notification/useDeleteNotification.ts
@@ -1,4 +1,4 @@
-import { InfiniteData, useMutation, UseMutationOptions, useQueryClient } from 'react-query';
+import { useMutation, UseMutationOptions, useQueryClient } from 'react-query';
 
 import { AxiosError, AxiosResponse } from 'axios';
 
@@ -17,22 +17,8 @@ const useDeleteNotification = (
 
   return useMutation(({ id }) => authFetcher.delete(`/notifications/${id}`), {
     ...options,
-    onSuccess: (_, variables) => {
-      queryClient.getQueryData(QUERY_KEYS.NOTIFICATIONS);
-      queryClient.setQueryData<InfiniteData<AxiosResponse<{ notifications: Notice[]; lastPage: boolean }>>>(
-        [QUERY_KEYS.NOTIFICATIONS, SIZE.NOTIFICATION_LOAD],
-        data =>
-          ({
-            pageParams: data?.pageParams,
-            pages: data?.pages.map(page => ({
-              ...page,
-              data: {
-                ...page.data,
-                notifications: page.data.notifications.filter(notice => notice.id !== variables.id),
-              },
-            })),
-          } as InfiniteData<AxiosResponse<{ notifications: Notice[]; lastPage: boolean }>>),
-      );
+    onSuccess: () => {
+      queryClient.refetchQueries([QUERY_KEYS.NOTIFICATIONS, SIZE.NOTIFICATION_LOAD]);
     },
     mutationKey: MUTATION_KEY.DELETE_NOTIFICATION,
   });

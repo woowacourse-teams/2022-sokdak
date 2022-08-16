4e28bfd9cb209bd006da4ee0ad524164947c3be9
JUDONGHYEOK <judonghyeok@gmail.com>
2022-08-16 22:24:19 +0900

    fix: 알림페이지에서 새로고침시 이상한 스낵바 메세지 렌더링되는 현상 해결

--- frontend/src/context/Auth.tsx
+++ frontend/src/context/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 
 import { STORAGE_KEY } from '@/constants/localStorage';
 import { parseJwt } from '@/utils/decodeJwt';
@@ -13,17 +13,20 @@ interface AuthContextValue {
 const AuthContext = React.createContext<AuthContextValue>({} as AuthContextValue);
 
 export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isLogin, setIsLogin] = useState(false);
-  const [username, setUserName] = useState('');
-
-  useEffect(() => {
-    const accessToken = localStorage.getItem(STORAGE_KEY.ACCESS_TOKEN);
-
+  const accessToken = localStorage.getItem(STORAGE_KEY.ACCESS_TOKEN);
+  const [isLogin, setIsLogin] = useState(() => {
     if (accessToken) {
-      setIsLogin(true);
-      setUserName(parseJwt(accessToken)?.nickname!);
+      return true;
     }
-  }, []);
+    return false;
+  });
+
+  const [username, setUserName] = useState(() => {
+    if (accessToken) {
+      return parseJwt(accessToken)?.nickname!;
+    }
+    return '';
+  });
 
   return <AuthContext.Provider value={{ isLogin, setIsLogin, username, setUserName }}>{children}</AuthContext.Provider>;
 };

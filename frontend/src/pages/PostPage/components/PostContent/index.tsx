bf4f2a5142422fd34b83d9207d73575ae68ee5a6
JUDONGHYEOK <judonghyeok@gmail.com>
2022-08-18 11:51:45 +0900

    feat: hashag 클릭시 해당 해시태그로 검색기능

--- frontend/src/pages/PostPage/components/PostContent/index.tsx
+++ frontend/src/pages/PostPage/components/PostContent/index.tsx
@@ -1,14 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 import HashTag from '@/components/HashTag';
 
 import * as Styled from './index.styles';
 
 const PostContent = ({ content, hashtags }: Pick<Post, 'hashtags' | 'content'>) => {
+  const navigate = useNavigate();
+
+  const handleHashtagClick = (name: string) => {
+    navigate(`/hashtag/${name}`);
+  };
+
   return (
     <Styled.ContentContainer>
       <Styled.Content>{content}</Styled.Content>
       <Styled.TagContainer>
         {hashtags?.map(({ name }) => (
-          <HashTag key={name} name={name} />
+          <HashTag key={name} name={name} handleTagClick={() => handleHashtagClick(name)} />
         ))}
       </Styled.TagContainer>
     </Styled.ContentContainer>

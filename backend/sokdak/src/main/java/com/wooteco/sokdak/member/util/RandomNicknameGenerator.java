02895db6a0b57405d47cbb111d88e94b79711e99
Chris <bjuuuu98@gmail.com>
2022-08-18 15:40:38 +0900

    style: 개행 수정

--- backend/sokdak/src/main/java/com/wooteco/sokdak/member/util/RandomNicknameGenerator.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/member/util/RandomNicknameGenerator.java
@@ -9,8 +9,9 @@ import java.util.stream.Collectors;
 public class RandomNicknameGenerator {
 
     private static final Random RANDOM = new Random();
-    private static final String[] ADJECTIVES = {"행복한", "즐거운", "슬픈", "화난", "기쁜", "울적한", "신기한", "재밌는", "속상한", "기쁜", "배고픈",
-            "걱정많은"};
+
+    private static final String[] ADJECTIVES =
+            {"행복한", "즐거운", "슬픈", "화난", "기쁜", "울적한", "신기한", "재밌는", "속상한", "기쁜", "배고픈", "걱정많은"};
     private static final String[] NOUNS = {"404", "200", "자스", "타스", "리액트", "스프링", "자바", "파이썬", "노드", "AWS", "맥북",
             "윈도우", "마우스", "키보드", "노드", "도커"};
     public static final Set<String> RANDOM_NICKNAMES = new HashSet<>();

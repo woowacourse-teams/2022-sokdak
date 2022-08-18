43cf40e0c9bafa8920254fd5e16cc9e5442e64cf
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-08-18 16:13:11 +0900

    fix: 닉네임 변경시 토큰을 재설정 하도록 변경

--- frontend/src/hooks/queries/profile/useUpdateNickname.ts
+++ frontend/src/hooks/queries/profile/useUpdateNickname.ts
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
 import { useMutation, UseMutationOptions } from 'react-query';
 
 import { AxiosError, AxiosResponse } from 'axios';
 
+import AuthContext from '@/context/Auth';
+
 import useSnackbar from '@/hooks/useSnackbar';
 
 import authFetcher from '@/apis';
+import { STORAGE_KEY } from '@/constants/localStorage';
+import SNACKBAR_MESSAGE from '@/constants/snackbar';
 
 interface UseUpdateNicknameProps {
   nickname: string;
@@ -13,6 +18,7 @@ interface UseUpdateNicknameProps {
 const useUpdateNickname = (
   options?: UseMutationOptions<AxiosResponse, AxiosError<ErrorResponse>, UseUpdateNicknameProps>,
 ) => {
+  const { setUserName } = useContext(AuthContext);
   const { showSnackbar } = useSnackbar();
 
   return useMutation(
@@ -27,6 +33,20 @@ const useUpdateNickname = (
           options.onError(error, variables, context);
         }
         showSnackbar(error.response?.data.message!);
+      },
+      onSuccess: (data, variables, context) => {
+        if (options && options.onSuccess) {
+          options.onSuccess(data, variables, context);
+        }
+
+        const accessToken = data?.headers.authorization;
+
+        if (accessToken) {
+          authFetcher.defaults.headers.common['Authorization'] = accessToken;
+          localStorage.setItem(STORAGE_KEY.ACCESS_TOKEN, accessToken);
+          setUserName(variables.nickname);
+          showSnackbar(SNACKBAR_MESSAGE.SUCCESS_UPDATE_NICKNAME);
+        }
       },
     },
   );

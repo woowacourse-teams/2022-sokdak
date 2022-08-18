75a45441829c6f5b5d1b5542b44ee875960075e7
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2022-08-18 16:24:09 +0900

    feat: 상세페이지에서 해시태그 클릭시 해당 검색페이지로 이동 (#517)
    * feat: hashag 클릭시 해당 해시태그로 검색기능
    
    * refactor: path 상수화

--- frontend/src/pages/PostPage/components/PostContent/index.tsx
+++ frontend/src/pages/PostPage/components/PostContent/index.tsx
@@ -1,14 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import HashTag from '@/components/HashTag';
 
 import * as Styled from './index.styles';
 
+import PATH from '@/constants/path';
+
 const PostContent = ({ content, hashtags }: Pick<Post, 'hashtags' | 'content'>) => {
+  const navigate = useNavigate();
+
+  const handleHashtagClick = (name: string) => {
+    navigate(`${PATH.HASHTAG}/${name}`);
+  };
+
   return (
     <Styled.ContentContainer>
       <Styled.Content>{content}</Styled.Content>
       <Styled.TagContainer>
         {hashtags?.map(({ name }) => (
-          <HashTag key={name} name={name} />
+          <HashTag key={name} name={name} handleTagClick={() => handleHashtagClick(name)} />
         ))}
       </Styled.TagContainer>
     </Styled.ContentContainer>

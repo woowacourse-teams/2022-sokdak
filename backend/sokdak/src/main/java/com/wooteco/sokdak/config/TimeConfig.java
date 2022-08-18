50bfe0023509010cd5e132160fa6a70a0989bdfb
Chris <bjuuuu98@gmail.com>
2022-08-18 15:47:00 +0900

    refactor: Springboot Timezone 설정 삭제

--- backend/sokdak/src/main/java/com/wooteco/sokdak/config/TimeConfig.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/config/TimeConfig.java
@@ -1,18 +1,11 @@
 package com.wooteco.sokdak.config;
 
 import java.time.Clock;
-import java.util.TimeZone;
-import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TimeConfig {
-
-    @PostConstruct
-    public void setTimeZone() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
 
     @Bean
     public Clock clock() {

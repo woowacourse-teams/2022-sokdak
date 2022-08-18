984a762e9c6f5b558fbff5d5131113d9ed57d32b
JUDONGHYEOK <judonghyeok@gmail.com>
2022-08-18 15:31:18 +0900

    refactor: path 상수화

--- frontend/src/pages/PostPage/components/PostContent/index.tsx
+++ frontend/src/pages/PostPage/components/PostContent/index.tsx
@@ -4,11 +4,13 @@ import HashTag from '@/components/HashTag';
 
 import * as Styled from './index.styles';
 
+import PATH from '@/constants/path';
+
 const PostContent = ({ content, hashtags }: Pick<Post, 'hashtags' | 'content'>) => {
   const navigate = useNavigate();
 
   const handleHashtagClick = (name: string) => {
-    navigate(`/hashtag/${name}`);
+    navigate(`${PATH.HASHTAG}/${name}`);
   };
 
   return (

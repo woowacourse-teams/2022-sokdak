dd7bfb611a89a6de0f860b48c1352e6c2a512a98
JUDONGHYEOK <judonghyeok@gmail.com>
2022-09-17 14:30:19 +0900

    fix: 폰트 파일 관련 테스트 오류 해결
    - ttf파일을 jest 환경에서 불러들이지 못하는 오류 발생
    - filemocking을 통해 해결

--- frontend/src/style/GlobalStyle.tsx
+++ frontend/src/style/GlobalStyle.tsx
@@ -1,5 +1,15 @@
 import emotionReset from 'emotion-reset';
 
+import '@/assets/fonts/BMHANNAAir/BMHANNAAir.ttf';
+import '@/assets/fonts/BMHANNAAir/BMHANNAAir.woff';
+import '@/assets/fonts/BMHANNAAir/BMHANNAAir.woff2';
+import '@/assets/fonts/BMHANNAPro/BMHANNAPro.ttf';
+import '@/assets/fonts/BMHANNAPro/BMHANNAPro.woff';
+import '@/assets/fonts/BMHANNAPro/BMHANNAPro.woff2';
+import '@/assets/fonts/BMYEONSUNG/BMYEONSUNG.ttf';
+import '@/assets/fonts/BMYEONSUNG/BMYEONSUNG.woff';
+import '@/assets/fonts/BMYEONSUNG/BMYEONSUNG.woff2';
+
 import { css, Global, keyframes } from '@emotion/react';
 
 const style = css`
@@ -7,9 +17,8 @@ const style = css`
 
   @font-face {
     font-family: 'BMYEONSUNG';
-    src: url(${require('@/assets/fonts/BMYEONSUNG/BMYEONSUNG.woff2')}) format('woff2'),
-      url(${require('@/assets/fonts/BMYEONSUNG/BMYEONSUNG.woff')}) format('woff'),
-      url(${require('@/assets/fonts/BMYEONSUNG/BMYEONSUNG.ttf')}) format('truetype');
+    src: url('static/BMYEONSUNG.woff2') format('woff2'), url('static/BMYEONSUNG.woff2') format('woff'),
+      url('static/BMYEONSUNG.woff2') format('truetype');
     font-weight: normal;
     font-style: normal;
     font-display: optional;
@@ -17,9 +26,8 @@ const style = css`
 
   @font-face {
     font-family: 'BMHANNAPro';
-    src: url(${require('@/assets/fonts/BMHANNAPro/BMHANNAPro.woff2')}) format('woff2'),
-      url(${require('@/assets/fonts/BMHANNAPro/BMHANNAPro.woff')}) format('woff'),
-      url(${require('@/assets/fonts/BMHANNAPro/BMHANNAPro.ttf')}) format('truetype');
+    src: url('static/BMHANNAPro.woff2') format('woff2'), url('static/BMHANNAPro.woff2') format('woff'),
+      url('static/BMHANNAPro.woff2') format('truetype');
     font-weight: normal;
     font-style: normal;
     font-display: optional;
@@ -27,9 +35,8 @@ const style = css`
 
   @font-face {
     font-family: 'BMHANNAAir';
-    src: url(${require('@/assets/fonts/BMHANNAAir/BMHANNAAir.woff2')}) format('woff2'),
-      url(${require('@/assets/fonts/BMHANNAAir/BMHANNAAir.woff')}) format('woff'),
-      url(${require('@/assets/fonts/BMHANNAAir/BMHANNAAir.ttf')}) format('truetype');
+    src: url('static/BMHANNAAir.woff2') format('woff2'), url('static/BMHANNAAir.woff2') format('woff'),
+      url('static/BMHANNAAir.woff2') format('truetype');
     font-weight: normal;
     font-style: normal;
     font-display: optional;

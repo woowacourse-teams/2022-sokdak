ecc35a19d17ff915597bc4c40ef1712155046d6f
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-09-19 21:10:52 +0900

    chore: bundle analyzer 추가

--- frontend/webpack/webpack.dev.js
+++ frontend/webpack/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 const { DefinePlugin } = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 require('dotenv').config();
 
@@ -10,6 +11,11 @@ module.exports = merge(common, {
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('https://devwas.sokdaksokdak.com'),
       'process.env.MODE': JSON.stringify('production'),
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      reportFilename: 'bundle-analyzer.html',
     }),
   ],
 });

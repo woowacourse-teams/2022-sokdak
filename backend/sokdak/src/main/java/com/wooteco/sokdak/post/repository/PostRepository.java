bbc7a5da8618cfd695c12be153968439bb4b6ab2
Chris <bjuuuu98@gmail.com>
2022-09-21 11:14:52 +0900

    refactor: PostRepository의 findSliceBy 삭제

--- backend/sokdak/src/main/java/com/wooteco/sokdak/post/repository/PostRepository.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/post/repository/PostRepository.java
@@ -4,14 +4,11 @@ import com.wooteco.sokdak.member.domain.Member;
 import com.wooteco.sokdak.post.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-    Slice<Post> findSliceBy(Pageable pageable);
 
     Page<Post> findPostsByMemberOrderByCreatedAtDesc(Pageable pageable, Member member);
 }

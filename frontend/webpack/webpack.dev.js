722d9f558a4937cf0e882a0221c8870707c21d2e
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-09-22 17:44:05 +0900

    chore: webpack 설정을 점검하고 최적화한다.  (#555)
    * chore: 캐시 버스팅을 위해 파일명에 hash 추가
    
    * chore: bundle analyzer 추가
    
    * chore: code splitting 적용과 함께 chunk splitting
    
    * chore: version 상수 제거
    
    * chore: 필요없는 CleanWebpackPlugin 제거
    
    * chore: chunk 이름 지정
    
    * fix: lazy loading 추가로 인한 테스트 깨짐 해경
    
    * chore: 필요없는TsconfigPathsPlugin  플러그인 제거
    
    * chore: dotenv 플러그인이 필요한 부분만 적용
    
    * chore: interpolate-html-plugin 제거
    
    * chore: clean-webpack-plugin 패키지 삭제
    
    * chore: url-loader 제거
    
    * chore: file-loader 제거
    
    * chore: ts-loader 제거
    
    * chore: esbuild-loader 적용

--- frontend/webpack/webpack.dev.js
+++ frontend/webpack/webpack.dev.js
@@ -1,8 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 const { DefinePlugin } = require('webpack');
-
-require('dotenv').config();
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'production',
@@ -10,6 +9,11 @@ module.exports = merge(common, {
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('https://devwas.sokdaksokdak.com'),
       'process.env.MODE': JSON.stringify('production'),
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      reportFilename: 'bundle-analyzer.html',
     }),
   ],
 });

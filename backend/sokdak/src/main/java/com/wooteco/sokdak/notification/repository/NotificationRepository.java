fa1184aa061a76096bb565d0bcc3379134691229
Chris <bjuuuu98@gmail.com>
2022-09-29 17:04:01 +0900

    style: sql 예약어 대문자로 수정

--- backend/sokdak/src/main/java/com/wooteco/sokdak/notification/repository/NotificationRepository.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/notification/repository/NotificationRepository.java
@@ -10,24 +10,24 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    @Query(value = "SELECT exists (SELECT * from Notification where member_id = :memberId and inquired = false)",
+    @Query(value = "SELECT EXISTS (SELECT * FROM Notification WHERE member_id = :memberId AND inquired = false)",
             nativeQuery = true)
     boolean existsByMemberIdAndInquiredIsFalse(Long memberId);
 
     @Modifying
-    @Query(value = "DELETE from Notification n WHERE n.id in :ids")
+    @Query(value = "DELETE FROM Notification n WHERE n.id IN :ids")
     void deleteAllById(List<Long> ids);
 
-    @Query(value = "SELECT n.id from Notification n where n.comment.id = :commentId")
+    @Query(value = "SELECT n.id FROM Notification n WHERE n.comment.id = :commentId")
     List<Long> findIdsByCommentId(Long commentId);
 
-    @Query(value = "SELECT n.id from Notification n where n.post.id = :postId")
+    @Query(value = "SELECT n.id FROM Notification n WHERE n.post.id = :postId")
     List<Long> findIdsByPostId(Long postId);
 
-    @Query(value = "SELECT n from Notification n where n.member.id = :memberId")
+    @Query(value = "SELECT n FROM Notification n WHERE n.member.id = :memberId")
     Slice<Notification> findNotificationsByMemberId(Long memberId, Pageable pageable);
 
     @Modifying
-    @Query(value = "UPDATE Notification n SET n.inquired = true WHERE n.id in :ids")
+    @Query(value = "UPDATE Notification n SET n.inquired = true WHERE n.id IN :ids")
     void inquireNotificationByIds(List<Long> ids);
 }

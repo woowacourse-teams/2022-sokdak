a3ba3c3e3ab5af68f56b8a6057e7039c5eb6a9eb
hyunrrr <whgusrms96@gmail.com>
2022-09-29 15:19:34 +0900

    refactor: 중복 신고 검사하는 로직 편의메서드에서 삭제
    - postReport 서비스에서 로직 구현

--- backend/sokdak/src/main/java/com/wooteco/sokdak/post/domain/Post.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/post/domain/Post.java
@@ -120,12 +120,6 @@ public class Post {
     }
 
     public void addReport(PostReport other) {
-        postReports.stream()
-                .filter(other::isSameReporter)
-                .findAny()
-                .ifPresent(it -> {
-                    throw new AlreadyReportPostException();
-                });
         postReports.add(other);
     }
 

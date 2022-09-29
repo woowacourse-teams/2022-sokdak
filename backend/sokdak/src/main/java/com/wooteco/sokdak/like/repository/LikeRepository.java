1c828c93c2371976154c631132ef3ad34334dfe0
Chris <bjuuuu98@gmail.com>
2022-09-29 15:28:04 +0900

    refactor: LikeRepository의 existsByMemberIdAndPostId native 쿼리로 변경

--- backend/sokdak/src/main/java/com/wooteco/sokdak/like/repository/LikeRepository.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/like/repository/LikeRepository.java
@@ -5,6 +5,7 @@ import com.wooteco.sokdak.member.domain.Member;
 import com.wooteco.sokdak.post.domain.Post;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
@@ -12,6 +13,8 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
 
     int countByPost(Post post);
 
+    @Query(value = "SELECT exists (SELECT * from likes WHERE member_id = :memberId and post_id = :postId)",
+            nativeQuery = true)
     boolean existsByMemberIdAndPostId(Long memberId, Long postId);
 
     void deleteAllByPost(Post post);

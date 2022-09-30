cc6ab88cf01331af1ee00da039c262c17588866a
JUDONGHYEOK <judonghyeok@gmail.com>
2022-09-30 16:41:19 +0900

    feat:Banner Hashtags loading api 연결

--- frontend/src/pages/MainPage/components/Banner/index.tsx
+++ frontend/src/pages/MainPage/components/Banner/index.tsx
@@ -1,10 +1,23 @@
+import useHashtags from '@/hooks/queries/hashtag/useHashtags';
 import useResponsive from '@/hooks/useResponsive';
 
 import * as Styled from './index.styles';
 
+const TRANSLATE_OBJ: { x: number; y: number }[] = [
+  { x: -140, y: -50 },
+  { x: 125, y: -50 },
+  { x: 60, y: 80 },
+  { x: 180, y: 30 },
+  { x: 0, y: 0 },
+  { x: -125, y: 40 },
+];
+
 const Banner = () => {
   const isOver = useResponsive(1100);
   const isTabletSizeOver = useResponsive(875);
+  const { data } = useHashtags({
+    storeCode: [10, ''],
+  });
 
   return (
     <>
@@ -21,36 +34,20 @@ const Banner = () => {
             </Styled.BannerText>
           </Styled.LeftSide>
           <Styled.HashTagsContainer>
-            <Styled.HashtagContainer x={-140} y={-50}>
-              <Styled.Hashtag>
-                <Styled.HashtagText>속닥속다고ㅅ갇거ㅏ거다ㅓㄱㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㄴㄴㄹ</Styled.HashtagText>
-              </Styled.Hashtag>
-            </Styled.HashtagContainer>
-            <Styled.HashtagContainer x={125} y={-50}>
-              <Styled.Hashtag>
-                <Styled.HashtagText>공식</Styled.HashtagText>
-              </Styled.Hashtag>
-            </Styled.HashtagContainer>
-            <Styled.HashtagContainer x={60} y={80}>
-              <Styled.Hashtag>
-                <Styled.HashtagText>공식</Styled.HashtagText>
-              </Styled.Hashtag>
-            </Styled.HashtagContainer>
-            <Styled.HashtagContainer x={isOver ? 180 : 100} y={30}>
-              <Styled.Hashtag>
-                <Styled.HashtagText>공식</Styled.HashtagText>
-              </Styled.Hashtag>
-            </Styled.HashtagContainer>
-            <Styled.HashtagContainer x={0} y={0}>
-              <Styled.Hashtag>
-                <Styled.HashtagText>공식</Styled.HashtagText>
-              </Styled.Hashtag>
-            </Styled.HashtagContainer>
-            <Styled.HashtagContainer x={-125} y={40}>
-              <Styled.Hashtag>
-                <Styled.HashtagText>공식</Styled.HashtagText>
-              </Styled.Hashtag>
-            </Styled.HashtagContainer>
+            {TRANSLATE_OBJ.map((item, idx) => {
+              if (!data?.data.hashtags || data?.data.hashtags.length <= idx) return;
+              return (
+                <Styled.HashtagContainer
+                  x={idx === 3 && !isOver ? 100 : item.x}
+                  y={item.y}
+                  key={data.data.hashtags[idx].name}
+                >
+                  <Styled.Hashtag>
+                    <Styled.HashtagText>{data.data.hashtags[idx].name}</Styled.HashtagText>
+                  </Styled.Hashtag>
+                </Styled.HashtagContainer>
+              );
+            })}
           </Styled.HashTagsContainer>
         </Styled.BannerContainer>
       ) : (

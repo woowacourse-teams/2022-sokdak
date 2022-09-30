9d748c104580807f0098f5b48ab6ba686a7c5e60
EastHShin <xldk78@gmail.com>
2022-09-30 20:11:38 +0900

    feat: request url 로깅 기능 구현

--- backend/sokdak/src/main/java/com/wooteco/sokdak/aspect/logging/LoggingAspect.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/aspect/logging/LoggingAspect.java
@@ -2,7 +2,10 @@ package com.wooteco.sokdak.aspect.logging;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -12,6 +15,12 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.CodeSignature;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Aspect
 @Component
@@ -19,24 +28,65 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final List<Class<? extends Annotation>> MAPPING_CLASSES = List
+            .of(RequestMapping.class, GetMapping.class, PostMapping.class,
+                    PutMapping.class, PatchMapping.class, DeleteMapping.class);
 
     @Pointcut("execution(public * com.wooteco.sokdak.*.controller.*.*(..)) && !@annotation(com.wooteco.sokdak.aspect.logging.NoLogging)")
     private void loggingCondition() {
     }
 
     @Before("loggingCondition()")
-    public void logRequest(JoinPoint joinPoint)  {
+    public void logRequest(JoinPoint joinPoint) {
         final Map<String, Object> parameters = extractParameters(joinPoint);
+        final Class<?> clazz = joinPoint.getTarget().getClass();
+        final String baseUrl = extractBaseUrl(clazz);
+
         final MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        final String methodName = signature.getMethod().getName();
+
+        final String requestUrl = MAPPING_CLASSES.stream()
+                .filter(mappingClass -> signature.getMethod().isAnnotationPresent(mappingClass))
+                .map(mappingClass -> getRequestUrl(signature.getMethod(), mappingClass, baseUrl))
+                .findAny()
+                .orElse("");
 
         try {
-            log.info("======= method: {}, body: {} ==========",
-                    methodName, objectMapper.writeValueAsString(parameters));
+            log.info("======= url: {}, body: {} ==========",
+                    requestUrl, objectMapper.writeValueAsString(parameters));
         } catch (JsonProcessingException exception) {
             log.warn("logging failed!!");
         }
     }
+
+    private String extractBaseUrl(Class<?> clazz) {
+        if (clazz.isAnnotationPresent(RequestMapping.class)){
+            final RequestMapping requestMapping = clazz.getDeclaredAnnotation(RequestMapping.class);
+            return extractPathValue(requestMapping.value());
+        }
+        return "";
+    }
+
+    private String getRequestUrl(Method method, Class<? extends Annotation> mappedAnnotation, String baseUrl){
+        final Annotation annotation = method.getAnnotation(mappedAnnotation);
+
+        try {
+            final String[] values = (String[]) mappedAnnotation.getMethod("value").invoke(annotation);
+            final String httpMethod = mappedAnnotation.getSimpleName().split("Mapping")[0].toUpperCase();
+            final String format = String.format("%s %s%s", httpMethod, baseUrl, extractPathValue(values));
+            return format;
+        } catch (Exception e){
+            log.warn("logging request url failed!!");
+            return null;
+        }
+    }
+
+    private String extractPathValue(String[] values) {
+        if (values.length > 0){
+            return values[0];
+        }
+        return "";
+    }
+
 
     private Map<String, Object> extractParameters(JoinPoint joinPoint) {
         final CodeSignature signature = (CodeSignature) joinPoint.getSignature();

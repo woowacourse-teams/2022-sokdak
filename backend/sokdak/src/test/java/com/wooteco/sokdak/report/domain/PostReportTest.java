227204371da145809866398595940cc71d4ff928
hyunrrr <whgusrms96@gmail.com>
2022-10-05 21:35:54 +0900

    feat: postReport 객체에 isOwner라는 메서드 구현

--- backend/sokdak/src/test/java/com/wooteco/sokdak/report/domain/PostReportTest.java
+++ backend/sokdak/src/test/java/com/wooteco/sokdak/report/domain/PostReportTest.java
@@ -10,9 +10,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.wooteco.sokdak.member.domain.Member;
 import com.wooteco.sokdak.post.domain.Post;
 import java.util.Collections;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 class PostReportTest {
@@ -45,5 +49,36 @@ class PostReportTest {
                 .build();
 
         assertThat(post.getPostReports()).contains(postReport);
+    }
+
+    @DisplayName("신고자가 맞으면 true, 아니면 false를 반환한다.")
+    @ParameterizedTest
+    @MethodSource("isOwnerArguments")
+    void isOwner(Member reporter, Member member, boolean expected) {
+        PostReport postReport = PostReport.builder()
+                .post(post)
+                .reporter(reporter)
+                .reportMessage("message")
+                .build();
+
+        assertThat(postReport.isOwner(member)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> isOwnerArguments() {
+        Member reporter = Member.builder()
+                .username("reporter")
+                .nickname("reporterNickname")
+                .password("Abcd123!@")
+                .build();
+        Member member = Member.builder()
+                .username("member")
+                .nickname("memberNickname")
+                .password("Abcd123!@")
+                .build();
+
+        return Stream.of(
+                Arguments.of(reporter, reporter, true),
+                Arguments.of(reporter, member, false)
+        );
     }
 }

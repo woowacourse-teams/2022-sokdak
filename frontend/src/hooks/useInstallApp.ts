bfcd590dfd41f271e090a6b4b7d18d6c172ea649
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-10-05 15:43:27 +0900

    fix: 비표준 API 타입 선언

--- frontend/src/hooks/useInstallApp.ts
+++ frontend/src/hooks/useInstallApp.ts
@@ -1,14 +1,28 @@
-// @ts-nocheck
 import { useState, useEffect, useRef } from 'react';
 
 import useSnackbar from '@/hooks/useSnackbar';
 
 import SNACKBAR_MESSAGE from '@/constants/snackbar';
 
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: Array<string>;
+  readonly userChoice: Promise<{
+    outcome: 'accepted' | 'dismissed';
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
+
+declare global {
+  interface WindowEventMap {
+    beforeinstallprompt: BeforeInstallPromptEvent;
+  }
+}
+
 const useInstallApp = () => {
   const { showSnackbar } = useSnackbar();
   const [installable, setInstallable] = useState(false);
-  const deferredPrompt = useRef(null);
+  const deferredPrompt = useRef<BeforeInstallPromptEvent | null>(null);
 
   useEffect(() => {
     window.addEventListener('beforeinstallprompt', e => {

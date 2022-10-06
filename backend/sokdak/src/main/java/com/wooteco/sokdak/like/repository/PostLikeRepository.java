4ab0d88ac7a17fe91023b6b3b4b2bb62cd309099
Chris <68771917+Byeongju-Kong@users.noreply.github.com>
2022-10-06 17:14:34 +0900

    fix: nativeQuery 삭제 (#618)

--- backend/sokdak/src/main/java/com/wooteco/sokdak/like/repository/PostLikeRepository.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/like/repository/PostLikeRepository.java
@@ -5,7 +5,6 @@ import com.wooteco.sokdak.member.domain.Member;
 import com.wooteco.sokdak.post.domain.Post;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
@@ -13,8 +12,6 @@ public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
     int countByPost(Post post);
 
-    @Query(value = "SELECT exists (SELECT * from post_likes WHERE member_id = :memberId and post_id = :postId)",
-            nativeQuery = true)
     boolean existsByMemberIdAndPostId(Long memberId, Long postId);
 
     void deleteAllByPost(Post post);

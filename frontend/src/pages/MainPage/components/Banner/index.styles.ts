90f43bb3807a7a8f10be944b33451ae2c3f0b45b
JUDONGHYEOK <judonghyeok@gmail.com>
2022-10-12 17:09:33 +0900

    style: banner style 변경

--- frontend/src/pages/MainPage/components/Banner/index.styles.ts
+++ frontend/src/pages/MainPage/components/Banner/index.styles.ts
@@ -5,7 +5,7 @@ import styled from '@emotion/styled';
 
 export const BannerContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   width: 100%;
   max-width: 1450px;
   cursor: default;

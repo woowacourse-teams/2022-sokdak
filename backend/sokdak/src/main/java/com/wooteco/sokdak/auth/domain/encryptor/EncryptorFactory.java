5ab72a9e64063dd068e274b7e064a29265f311b8
BETTERFUTURE4 <rerub0831@gmail.com>
2022-10-14 20:08:47 +0900

    refactor: 암호화 빈 등록 메소드에서 static 제거

--- backend/sokdak/src/main/java/com/wooteco/sokdak/auth/domain/encryptor/EncryptorFactory.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/auth/domain/encryptor/EncryptorFactory.java
@@ -7,15 +7,19 @@ import org.springframework.context.annotation.Configuration;
 public class EncryptorFactory {
 
     private static final SHA256 SHA_256 = new SHA256();
-    private static final Encryptor ENCRYPTOR = new Encryptor(sha256());
+    private static final Encryptor ENCRYPTOR = new Encryptor(SHA_256);
 
-    @Bean
     public static Encryptor encryptor() {
         return ENCRYPTOR;
     }
 
     @Bean
-    public static Encoder sha256() {
+    public Encryptor getEncryptor() {
+        return ENCRYPTOR;
+    }
+
+    @Bean
+    public Encoder getSha256() {
         return SHA_256;
     }
 }

a6005dfcccace4d444c87df4cb9c2a8ae0ffed2c
JUDONGHYEOK <judonghyeok@gmail.com>
2022-10-16 21:50:18 +0900

    style: postList 스타일 조정

--- frontend/src/components/PostList/index.styles.ts
+++ frontend/src/components/PostList/index.styles.ts
@@ -3,16 +3,18 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(auto-fit, minmax(min(525px, 100%), 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(min(525px, 50%), 1fr));
   align-items: center;
   gap: 2em;
   position: relative;
   justify-items: center;
+  @media (max-width: 1250px) and (min-width: 875px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 export const PostItemContainer = styled.div`
   width: 100%;
   box-sizing: border-box;
-  max-width: 900px;
   min-width: 320px;
 `;

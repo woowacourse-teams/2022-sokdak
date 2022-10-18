c355a74180a0c4951adf2d21bf5a1ca024deb6ca
hyunrrr <whgusrms96@gmail.com>
2022-10-18 11:25:59 +0900

    refactor: boardId를 comment로부터 가져오도록 변경

--- backend/sokdak/src/test/java/com/wooteco/sokdak/report/service/CommentReportServiceTest.java
+++ backend/sokdak/src/test/java/com/wooteco/sokdak/report/service/CommentReportServiceTest.java
@@ -3,6 +3,9 @@ package com.wooteco.sokdak.report.service;
 import static com.wooteco.sokdak.member.domain.RoleType.USER;
 import static com.wooteco.sokdak.util.fixture.BoardFixture.APPLICANT_BOARD_ID;
 import static com.wooteco.sokdak.util.fixture.BoardFixture.FREE_BOARD_ID;
+import static com.wooteco.sokdak.util.fixture.BoardFixture.GOOD_CREW_BOARD_ID;
+import static com.wooteco.sokdak.util.fixture.BoardFixture.HOT_BOARD_ID;
+import static com.wooteco.sokdak.util.fixture.BoardFixture.POSUTA_BOARD_ID;
 import static com.wooteco.sokdak.util.fixture.CommentFixture.VALID_COMMENT_MESSAGE;
 import static com.wooteco.sokdak.util.fixture.MemberFixture.VALID_NICKNAME;
 import static com.wooteco.sokdak.util.fixture.PostFixture.VALID_POST_CONTENT;
@@ -10,9 +13,15 @@ import static com.wooteco.sokdak.util.fixture.PostFixture.VALID_POST_TITLE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.wooteco.sokdak.auth.dto.AuthInfo;
 import com.wooteco.sokdak.auth.exception.AuthorizationException;
+import com.wooteco.sokdak.board.domain.Board;
+import com.wooteco.sokdak.board.domain.PostBoard;
+import com.wooteco.sokdak.board.repository.BoardRepository;
+import com.wooteco.sokdak.board.repository.PostBoardRepository;
 import com.wooteco.sokdak.comment.domain.Comment;
 import com.wooteco.sokdak.comment.exception.CommentNotFoundException;
 import com.wooteco.sokdak.comment.repository.CommentRepository;
@@ -25,13 +34,15 @@ import com.wooteco.sokdak.report.exception.AlreadyReportCommentException;
 import com.wooteco.sokdak.report.exception.InvalidReportMessageException;
 import com.wooteco.sokdak.report.repository.CommentReportRepository;
 import com.wooteco.sokdak.util.ServiceTest;
-import com.wooteco.sokdak.util.fixture.BoardFixture;
 import java.util.Collections;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class CommentReportServiceTest extends ServiceTest {
@@ -53,10 +64,20 @@ class CommentReportServiceTest extends ServiceTest {
     @Autowired
     private NotificationRepository notificationRepository;
 
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private PostBoardRepository postBoardRepository;
+
     private Comment comment;
+    private Comment applicantComment;
 
     @BeforeEach
     void setUp() {
+        Board freeBoard = boardRepository.findById(FREE_BOARD_ID).get();
+        Board applicantBoard = boardRepository.findById(APPLICANT_BOARD_ID).get();
+
         Post post = Post.builder()
                 .member(member)
                 .title(VALID_POST_TITLE)
@@ -66,6 +87,24 @@ class CommentReportServiceTest extends ServiceTest {
                 .postLikes(Collections.emptyList())
                 .build();
         postRepository.save(post);
+        PostBoard postBoard = PostBoard.builder().build();
+        postBoard.addPost(post);
+        postBoard.addBoard(freeBoard);
+        postBoardRepository.save(postBoard);
+
+        Post applicantPost = Post.builder()
+                .member(member)
+                .title(VALID_POST_TITLE)
+                .content(VALID_POST_CONTENT)
+                .postHashtags(Collections.emptyList())
+                .comments(Collections.emptyList())
+                .postLikes(Collections.emptyList())
+                .build();
+        postRepository.save(applicantPost);
+        PostBoard applicantPostBoard = PostBoard.builder().build();
+        applicantPostBoard.addBoard(applicantBoard);
+        applicantPostBoard.addPost(applicantPost);
+        postBoardRepository.save(applicantPostBoard);
 
         comment = Comment.builder()
                 .member(member)
@@ -74,6 +113,14 @@ class CommentReportServiceTest extends ServiceTest {
                 .message(VALID_COMMENT_MESSAGE)
                 .build();
         commentRepository.save(comment);
+
+        applicantComment = Comment.builder()
+                .member(member)
+                .post(applicantPost)
+                .nickname(VALID_NICKNAME)
+                .message(VALID_COMMENT_MESSAGE)
+                .build();
+        commentRepository.save(applicantComment);
     }
 
     @DisplayName("댓글 신고 기능")
@@ -145,26 +192,50 @@ class CommentReportServiceTest extends ServiceTest {
 
     @DisplayName("지원자는 권한이 없는 게시판 게시글의 댓글, 대댓글을 신고할 수 없다.")
     @ParameterizedTest
-    @CsvSource({"1", "2", "3", "4"})
-    void flipPostLike_Applicant_Exception(Long boardId) {
-        ReportRequest reportRequest = new ReportRequest(boardId, "message");
+    @MethodSource("mockComments")
+    void flipPostLike_Applicant_Exception(Comment mockComment) {
+        ReportRequest reportRequest = new ReportRequest(mockComment.getBoardId(), "message");
 
-        assertThatThrownBy(() -> commentReportService.reportComment(comment.getId(), reportRequest, APPLICANT_AUTH_INFO))
+        assertThatThrownBy(() -> commentReportService.reportComment(mockComment.getId(), reportRequest, APPLICANT_AUTH_INFO))
                 .isInstanceOf(AuthorizationException.class);
+    }
+
+    static Stream<Arguments> mockComments() {
+        Comment hotBoardComment = mock(Comment.class);
+        Comment freeBoardComment = mock(Comment.class);
+        Comment posutaBoardComment = mock(Comment.class);
+        Comment goodCrewBoardComment = mock(Comment.class);
+
+        when(hotBoardComment.getBoardId()).thenReturn(HOT_BOARD_ID);
+        when(freeBoardComment.getBoardId()).thenReturn(FREE_BOARD_ID);
+        when(posutaBoardComment.getBoardId()).thenReturn(POSUTA_BOARD_ID);
+        when(goodCrewBoardComment.getBoardId()).thenReturn(GOOD_CREW_BOARD_ID);
+
+        when(hotBoardComment.getId()).thenReturn(1L);
+        when(freeBoardComment.getId()).thenReturn(1L);
+        when(posutaBoardComment.getId()).thenReturn(1L);
+        when(goodCrewBoardComment.getId()).thenReturn(1L);
+
+        return Stream.of(
+                Arguments.of(hotBoardComment),
+                Arguments.of(freeBoardComment),
+                Arguments.of(posutaBoardComment),
+                Arguments.of(goodCrewBoardComment)
+        );
     }
 
     @DisplayName("지원자는 권한이 있는 게시판 게시글의 댓글 대댓글을 신고할 수 있다.")
     @Test
     void flipPostLike_Applicant() {
         ReportRequest reportRequest = new ReportRequest(APPLICANT_BOARD_ID, "message");
-        int reportCountBeforeReport = comment.getCommentReports().size();
+        int reportCountBeforeReport = applicantComment.getCommentReports().size();
 
-        commentReportService.reportComment(comment.getId(), reportRequest, APPLICANT_AUTH_INFO);
+        commentReportService.reportComment(applicantComment.getId(), reportRequest, APPLICANT_AUTH_INFO);
 
-        Post post = postRepository.findById(this.comment.getId())
+        Post post = postRepository.findById(this.applicantComment.getId())
                 .orElseThrow(PostNotFoundException::new);
 
-        int reportCountAfterReport = comment.getCommentReports().size();
+        int reportCountAfterReport = applicantComment.getCommentReports().size();
 
         assertThat(reportCountBeforeReport + 1).isEqualTo(reportCountAfterReport);
     }

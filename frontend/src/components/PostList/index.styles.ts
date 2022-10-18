f02bfd46ce15eb3bdbf5ca43d7a7ae50c20cb467
JUDONGHYEOK <judonghyeok@gmail.com>
2022-10-18 17:51:28 +0900

    style: 목록 페이지 grid 조정

--- frontend/src/components/PostList/index.styles.ts
+++ frontend/src/components/PostList/index.styles.ts
@@ -3,12 +3,13 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(auto-fit, minmax(min(525px, 50%), 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(min(500px, 50%), 1fr));
   align-items: center;
   gap: 2em;
   position: relative;
   justify-items: center;
-  @media (max-width: 1250px) and (min-width: 875px) {
+
+  @media (max-width: 1290px) and (min-width: 875px) {
     grid-template-columns: repeat(2, 1fr);
   }
 `;

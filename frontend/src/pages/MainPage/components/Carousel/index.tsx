85718fc041f6ef4eedaa2e4832bb4a584ae3a4b7
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2022-10-18 17:56:37 +0900

    refactor: 캐러셀 로직 개선 및 목록페이지 ui 개선 (#674)
    * refactor: carousel 로직 개선
    
    * style: 목록 페이지 grid 조정
    
    * style: 캐러셀 scrollbar 안보이게 하기

--- frontend/src/pages/MainPage/components/Carousel/index.tsx
+++ frontend/src/pages/MainPage/components/Carousel/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import PostListItem from '@/components/PostListItem';
@@ -10,23 +10,22 @@ import * as Styled from './index.styles';
 
 import PATH from '@/constants/path';
 
-import useCarousel from './useCarousel';
-
 const Carousel = () => {
-  const [page, setPage] = useState(0);
   const { data } = useHotPosts({ storeCode: [10], options: { suspense: true, staleTime: 1000 * 40 } });
   const navigate = useNavigate();
+  const scrollContainer = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
-  const { ref, isEnd, setIsEnd } = useCarousel();
   const handleClickLastPage = useThrottle(() => {
-    if (isEnd) {
-      setIsEnd(false);
+    if (scrollContainer.current && ref.current) {
+      scrollContainer.current.scrollLeft -= ref.current.clientWidth;
     }
-    setPage(prev => prev - 1);
   }, 300);
 
   const handleClickNextPage = useThrottle(() => {
-    setPage(prev => prev + 1);
+    if (scrollContainer.current && ref.current) {
+      scrollContainer.current.scrollLeft += ref.current.clientWidth;
+    }
   }, 500);
 
   const handleClickPostItem = (id: number) => {
@@ -38,25 +37,23 @@ const Carousel = () => {
       {data?.posts.length !== 0 && (
         <Styled.Container>
           <Styled.Title>🔥 핫 게시판 🔥</Styled.Title>
-          {page !== 0 && <Styled.ArrowLeft onClick={handleClickLastPage}>{'<'}</Styled.ArrowLeft>}
-          {!isEnd && <Styled.ArrowRight onClick={handleClickNextPage}>{'>'}</Styled.ArrowRight>}
+          <Styled.ArrowLeft onClick={handleClickLastPage}>{'<'}</Styled.ArrowLeft>
+          <Styled.ArrowRight onClick={handleClickNextPage}>{'>'}</Styled.ArrowRight>
 
           <Styled.CarouselContainer>
-            <Styled.PostContainer>
-              <Styled.PostListContainer page={page}>
-                {data?.posts.map((item, idx) => (
-                  <Styled.ItemContainer key={item.id} ref={idx === data.posts.length - 1 ? ref : null}>
-                    <PostListItem
-                      {...item}
-                      handleClick={() => {
-                        handleClickPostItem(item.id);
-                      }}
-                      testid={item.id}
-                    />
-                  </Styled.ItemContainer>
-                ))}
-              </Styled.PostListContainer>
-            </Styled.PostContainer>
+            <Styled.PostListContainer ref={scrollContainer}>
+              {data?.posts.map((item, idx) => (
+                <Styled.ItemContainer key={item.id} ref={idx === data.posts.length - 1 ? ref : null}>
+                  <PostListItem
+                    {...item}
+                    handleClick={() => {
+                      handleClickPostItem(item.id);
+                    }}
+                    testid={item.id}
+                  />
+                </Styled.ItemContainer>
+              ))}
+            </Styled.PostListContainer>
           </Styled.CarouselContainer>
         </Styled.Container>
       )}

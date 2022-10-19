2ff1650a5d5ab3d9954e1bdc92e7a0fdf5c54848
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-10-19 23:04:35 +0900

    feat: 글 검색 MSW handler 구현

--- frontend/src/mocks/handlers/posts/index.ts
+++ frontend/src/mocks/handlers/posts/index.ts
@@ -212,9 +212,40 @@ const postHandlers = [
 
   rest.get('/posts', (req, res, ctx) => {
     const hashtagName = req.url.searchParams.get('hashtag');
+    const query = req.url.searchParams.get('query');
     const size = Number(req.url.searchParams.get('size')!);
     const page = Number(req.url.searchParams.get('page')!);
 
+    if (hashtagName === null && query !== null) {
+      // 쿼리 검색일 경우
+      const keywords = query.split('|');
+      const result = [];
+
+      for (const post of postList) {
+        let include = false;
+
+        for (const keyword of keywords) {
+          if (post.title.includes(keyword)) {
+            include = true;
+            break;
+          }
+          if (post.content.includes(keyword)) {
+            include = true;
+          }
+        }
+
+        if (include) {
+          result.push(post);
+        }
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({ posts: result.slice(page * size, page * size + size), totalPostCount: result.length }),
+      );
+    }
+
+    // 해시태그 검색일 경우
     const postsByHashtag = postList.filter(post => post.hashtags.some(hashtag => hashtag.name === hashtagName));
 
     if (postsByHashtag.length <= 0) {
@@ -231,6 +262,7 @@ const postHandlers = [
       }),
     );
   }),
+
   rest.post<{ message: string }>('/posts/:id/report', (req, res, ctx) => {
     const { id } = req.params;
     const { message } = req.body;

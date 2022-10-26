26c5ff13e299dfd7ee7e04f13da76e8fb06474f7
JUDONGHYEOK <judonghyeok@gmail.com>
2022-10-26 13:34:19 +0900

    chore: hastags 네이밍 hastag로 변경

--- frontend/src/hooks/queries/hashtag/useSearchHashtags.ts
+++ frontend/src/hooks/queries/hashtag/useSearchHashtags.ts
@@ -2,7 +2,7 @@ import { useQuery, QueryKey, UseQueryOptions } from 'react-query';
 
 import { AxiosError } from 'axios';
 
-import { requestGetHashTags } from '@/apis/hashtags';
+import { requestGetHashTags } from '@/apis/hashtag';
 import QUERY_KEYS from '@/constants/queries';
 
 type Limit = number;

2a20bf394e47687bcadd79c71faef68610215160
JUDONGHYEOK <judonghyeok@gmail.com>
2022-10-31 16:24:48 +0900

    chore: getBoard interface 이름 변경

--- frontend/src/api/boards.ts
+++ frontend/src/api/boards.ts
@@ -1,11 +1,11 @@
 import api from '.';
 
-export interface BoardsResponse {
+export interface GetBoardsResponse {
   boards: Board[];
 }
 
 export const requestGetBoards = async () => {
-  const { data } = await api.get<BoardsResponse>('/boards');
+  const { data } = await api.get<GetBoardsResponse>('/boards');
 
   return data.boards;
 };

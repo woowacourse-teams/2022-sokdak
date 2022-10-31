e238dd68088f19c011db284c0aef47279e01ffd0
JUDONGHYEOK <judonghyeok@gmail.com>
2022-10-31 16:07:48 +0900

    refactor: api board 타입 선언

--- frontend/src/api/boards.ts
+++ frontend/src/api/boards.ts
@@ -1,7 +1,11 @@
 import api from '.';
 
+export interface BoardsResponse {
+  boards: Board[];
+}
+
 export const requestGetBoards = async () => {
-  const { data } = await api.get('/boards');
+  const { data } = await api.get<BoardsResponse>('/boards');
 
   return data.boards;
 };
--- frontend/src/hooks/queries/board/useBoards.ts
+++ frontend/src/hooks/queries/board/useBoards.ts
@@ -5,11 +5,7 @@ import { AxiosError } from 'axios';
 import { requestGetBoards } from '@/api/boards';
 import QUERY_KEYS from '@/constants/queries';
 
-interface BoardsResponse {
-  boards: Board[];
-}
-
-const useBoards = ({ options }: { options?: UseQueryOptions<BoardsResponse, AxiosError, Board[], string> }) =>
+const useBoards = ({ options }: { options?: UseQueryOptions<Board[], AxiosError, Board[], string> }) =>
   useQuery(QUERY_KEYS.BOARDS, () => requestGetBoards(), {
     staleTime: Infinity,
     ...options,

2e3606161b0ed99d35ff7758b7e44fc854058737
JUDONGHYEOK <judonghyeok@gmail.com>
2022-10-31 16:26:46 +0900

    refactor: hashtag api 타입 지정

--- frontend/src/api/hashtag.ts
+++ frontend/src/api/hashtag.ts
@@ -1,7 +1,11 @@
 import api from '.';
 
+export interface GetHashTagsResponse {
+  hashtags: Hashtag[];
+}
+
 export const requestGetHashTags = async (limit: number, include: string) => {
-  const { data } = await api.get(`/hashtags/popular?limit=${limit}&include=${include}`);
+  const { data } = await api.get<GetHashTagsResponse>(`/hashtags/popular?limit=${limit}&include=${include}`);
 
   return data;
 };
--- frontend/src/hooks/queries/hashtag/useSearchHashtags.ts
+++ frontend/src/hooks/queries/hashtag/useSearchHashtags.ts
@@ -3,6 +3,7 @@ import { useQuery, QueryKey, UseQueryOptions } from 'react-query';
 import { AxiosError } from 'axios';
 
 import { requestGetHashTags } from '@/api/hashtag';
+import type { GetHashTagsResponse } from '@/api/hashtag';
 import QUERY_KEYS from '@/constants/queries';
 
 type Limit = number;
@@ -13,7 +14,7 @@ const useSearchHashtags = ({
   options,
 }: {
   storeCode: [Limit, Include];
-  options?: UseQueryOptions<{ hashtags: Hashtag[] }, AxiosError, { hashtags: Hashtag[] }, [QueryKey, Limit, Include]>;
+  options?: UseQueryOptions<GetHashTagsResponse, AxiosError, GetHashTagsResponse, [QueryKey, Limit, Include]>;
 }) => {
   return useQuery(
     [QUERY_KEYS.HASHTAGS, ...storeCode],

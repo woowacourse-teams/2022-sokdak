c55c7e505791bcf31f5eb48cee44eb5547d1971e
Chris <bjuuuu98@gmail.com>
2022-11-22 13:26:23 +0900

    refactor: NewNotificationEventHandlerTest가 ServiceTest를 상속하지 않도록 수정

--- backend/sokdak/src/test/java/com/wooteco/sokdak/notification/handler/NewNotificationEventHandlerTest.java
+++ backend/sokdak/src/test/java/com/wooteco/sokdak/notification/handler/NewNotificationEventHandlerTest.java
@@ -2,21 +2,39 @@ package com.wooteco.sokdak.notification.handler;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+<<<<<<< HEAD
 import com.wooteco.sokdak.comment.domain.NewCommentEvent;
 import com.wooteco.sokdak.event.NotificationEvent;
+=======
+import com.wooteco.sokdak.board.event.PostHotBoardEvent;
+import com.wooteco.sokdak.comment.event.NewCommentEvent;
+import com.wooteco.sokdak.comment.event.NewReplyEvent;
+import com.wooteco.sokdak.config.AsyncTestConfig;
+>>>>>>> e7e77f05 (refactor: NewNotificationEventHandlerTest가 ServiceTest를 상속하지 않도록 수정)
 import com.wooteco.sokdak.notification.repository.NotificationRepository;
 import com.wooteco.sokdak.report.domain.PostReportEvent;
 import com.wooteco.sokdak.util.DatabaseCleaner;
+<<<<<<< HEAD
 import com.wooteco.sokdak.util.ServiceTest;
 import java.util.stream.Stream;
+=======
+>>>>>>> e7e77f05 (refactor: NewNotificationEventHandlerTest가 ServiceTest를 상속하지 않도록 수정)
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+<<<<<<< HEAD
+=======
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+>>>>>>> e7e77f05 (refactor: NewNotificationEventHandlerTest가 ServiceTest를 상속하지 않도록 수정)
 
-class NewNotificationEventHandlerTest extends ServiceTest {
+@SpringBootTest
+@Import(AsyncTestConfig.class)
+class NewNotificationEventHandlerTest {
 
     private static final long POST_ID = 1L;
 

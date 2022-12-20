26946e98f0319bdd1189943cabe30663e51d0970
Chris <bjuuuu98@gmail.com>
2022-12-20 20:46:17 +0900

    refactor: 알림 삭제 로직을 동기 이벤트 방식으로 수정

--- backend/sokdak/src/main/java/com/wooteco/sokdak/notification/handler/DeletionEventHandler.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/notification/handler/DeletionEventHandler.java
@@ -3,15 +3,12 @@ package com.wooteco.sokdak.notification.handler;
 import com.wooteco.sokdak.comment.event.CommentDeletionEvent;
 import com.wooteco.sokdak.notification.repository.NotificationRepository;
 import com.wooteco.sokdak.post.event.PostDeletionEvent;
-import org.springframework.scheduling.annotation.Async;
+import javax.transaction.Transactional;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
-@Async("asyncExecutor")
-@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Transactional
 public class DeletionEventHandler {
 
     private final NotificationRepository notificationRepository;
@@ -20,12 +17,12 @@ public class DeletionEventHandler {
         this.notificationRepository = notificationRepository;
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void handleCommentDeletion(CommentDeletionEvent commentDeletionEvent) {
         notificationRepository.deleteAllByCommentId(commentDeletionEvent.getCommentId());
     }
 
-    @TransactionalEventListener
+    @EventListener
     public void handlePostDeletion(PostDeletionEvent postDeletionEvent) {
         notificationRepository.deleteAllByPostId(postDeletionEvent.getPostId());
     }

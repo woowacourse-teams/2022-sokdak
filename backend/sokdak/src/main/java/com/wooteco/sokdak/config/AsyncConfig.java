4675b91497605909b8bfdbcc8b9e42f1007b0636
Chris <bjuuuu98@gmail.com>
2023-08-11 14:31:13 +0900

    refactor: 비동기 쓰레드풀을 ThreadPoolTaskExecutor에서 CachedThreadPool로 변경

--- backend/sokdak/src/main/java/com/wooteco/sokdak/config/AsyncConfig.java
+++ backend/sokdak/src/main/java/com/wooteco/sokdak/config/AsyncConfig.java
@@ -1,12 +1,13 @@
 package com.wooteco.sokdak.config;
 
-import java.util.concurrent.Executor;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
@@ -15,13 +16,7 @@ public class AsyncConfig implements AsyncConfigurer {
     @Override
     @Bean(name = "asyncExecutor")
     public Executor getAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(5);
-        executor.setQueueCapacity(10);
-        executor.setThreadNamePrefix("asyncExecutor");
-        executor.initialize();
-        return executor;
+        return Executors.newCachedThreadPool();
     }
 
     @Override
